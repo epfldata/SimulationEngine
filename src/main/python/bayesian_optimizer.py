145a6c542b8b10c1d21f46a0db4023da89f8d59e
Valentin Rupprecht <valentin.rupprecht@yahoo.de>
2019-07-25 14:28:04 +0200

    call sbt package

--- src/main/python/bayesian_optimizer.py
+++ src/main/python/bayesian_optimizer.py
@@ -2,13 +2,14 @@ from bayes_opt import BayesianOptimization
 import subprocess
 import os, sys
 
+os.chdir('../../..')  # going to the root of the project
 
-os.chdir('../../..')    #going to the root of the project
 
 def black_box_function(a, b, c):
     result = runCmd("scala target/scala-2.11/economic_simulations_2.11-1.0.jar evaluate")
     print(result)
     return -float(result.decode("utf-8")[:-1])
+
 
 def runCmd(cmd):
     print("Run command:", cmd)
@@ -21,9 +22,9 @@ def runCmd(cmd):
         sys.exit(rc)
     return result
 
+
 runCmd("sbt clean package")
 runCmd("scala target/scala-2.11/economic_simulations_2.11-1.0.jar generate")
 pbounds = {'a': (0, 1), 'b': (0, 1), 'c': (0, 1)}
 optimizer = BayesianOptimization(black_box_function, pbounds, 10)
-optimizer.maximize(init_points = 5, n_iter = 25)
-
+optimizer.maximize(init_points=5, n_iter=25)
