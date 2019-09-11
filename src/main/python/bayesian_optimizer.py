efb03642ee01ac0555cb01318c2f03811ffc483c
Valentin Rupprecht <valentin.rupprecht@yahoo.de>
2019-09-11 12:01:37 +0200

    simplified bayesian optimization

--- src/main/python/bayesian_optimizer.py
+++ src/main/python/bayesian_optimizer.py
@@ -61,45 +61,46 @@ if __name__ == '__main__':
     params = json.loads(f.read())
     f.close()
 
-    runCmd("sbt clean compile")
+    pbounds = {}
+    constants = params["constants"]
+    variables = params["variables"]
+    for agentType in constants:
+        append = {
+            "number": (1, 200) if agentType == "Person" else (1, 10),
+            **{gender: (0, 1) for gender in constants[agentType] if "gender" in gender.lower()},
+            **{units: (1, 10) for units in constants[agentType] if units in ["buyMu", "buySigma", "consumeMu", "consumeSigma"]},
+            **{buyFood: (0, 1) for buyFood in constants[agentType] if "buy" in buyFood.lower() and buyFood not in ["buyMu", "buySigma"]},
+            **{consumeFood: (0, 1) for consumeFood in constants[agentType] if "consume" in consumeFood.lower()
+               and consumeFood not in ["consumeMu", "consumeSigma"]},
+            **{enjoyment: (0, 1000) for enjoyment in constants[agentType] if "enjoy" in enjoyment.lower()},
+            **{edu: (0, 10) for edu in constants[agentType] if "edu" in edu.lower()},
+            **{bonusSal: (0, 1000) for bonusSal in constants[agentType] if "bonussal" in bonusSal.lower()},
+            **{salary: (10 ** 4, 10 ** 6) for salary in constants[agentType] if "salary" in salary.lower()},
+            **{iters: (0, 20) for iters in constants[agentType] if "iters" in iters.lower()},
+            **{plUnits: (1, 20) for plUnits in constants[agentType] if plUnits in ["required", "consumed", "produced", "time"]}
+        }
+        pbounds = {**pbounds, **{("constants-{}-{}".format(agentType, key)): append[key] for key in append}}
+    for agentType in variables:
+        append = {
+            **{capital: (0, 10000) for capital in variables[agentType] if "capital" in capital.lower()},
+            **{value_destroyed: (0, 100) for value_destroyed in variables[agentType] if "value_destroyed" in value_destroyed.lower()},
+            **{happiness: (0, 100) for happiness in variables[agentType] if "happiness" in happiness.lower()},
+            **{salary: (10 ** 4, 10 ** 6) for salary in variables[agentType] if "salary" in salary.lower()}
+        }
+        pbounds = {**pbounds, **{("variables-{}-{}".format(agentType, key)): append[key] for key in append}}
+
     stepSizes = [20, 50, 100]
     sampleSize = 5
+    nSteps = 1
+    optimization_iters = 20
     params_result = {"stepSize-{}".format(stepSize): {"entry-{}".format(entry): {} for entry in range(sampleSize)} for stepSize in stepSizes}
-    for stepSize in stepSizes:
-        for entry in range(sampleSize):
-            nSteps = 1
-            runCmd('sbt "run generate {} {} {} {}"'.format(json_original, sampleSize, nSteps, stepSize))
-            pbounds = {}
-            constants = params["constants"]
-            variables = params["variables"]
-            for agentType in constants:
-                append = {
-                    "number": (1, 200) if agentType == "Person" else (1, 10),
-                    **{gender: (0, 1) for gender in constants[agentType] if "gender" in gender.lower()},
-                    **{units: (1, 10) for units in constants[agentType] if units in ["buyMu", "buySigma", "consumeMu", "consumeSigma"]},
-                    **{buyFood: (0, 1) for buyFood in constants[agentType] if "buy" in buyFood.lower() and buyFood not in ["buyMu", "buySigma"]},
-                    **{consumeFood: (0, 1) for consumeFood in constants[agentType] if "consume" in consumeFood.lower()
-                       and consumeFood not in ["consumeMu", "consumeSigma"]},
-                    **{enjoyment: (0, 1000) for enjoyment in constants[agentType] if "enjoy" in enjoyment.lower()},
-                    **{edu: (0, 10) for edu in constants[agentType] if "edu" in edu.lower()},
-                    **{bonusSal: (0, 1000) for bonusSal in constants[agentType] if "bonussal" in bonusSal.lower()},
-                    **{salary: (10 ** 4, 10 ** 6) for salary in constants[agentType] if "salary" in salary.lower()},
-                    **{iters: (0, 20) for iters in constants[agentType] if "iters" in iters.lower()},
-                    **{plUnits: (1, 20) for plUnits in constants[agentType] if plUnits in ["required", "consumed", "produced", "time"]}
-                }
-                pbounds = {**pbounds, **{("constants-{}-{}".format(agentType, key)): append[key] for key in append}}
-            for agentType in variables:
-                append = {
-                    **{capital: (0, 10000) for capital in variables[agentType] if "capital" in capital.lower()},
-                    **{value_destroyed: (0, 100) for value_destroyed in variables[agentType] if "value_destroyed" in value_destroyed.lower()},
-                    **{happiness: (0, 100) for happiness in variables[agentType] if "happiness" in happiness.lower()},
-                    **{salary: (10 ** 4, 10 ** 6) for salary in variables[agentType] if "salary" in salary.lower()}
-                }
-                pbounds = {**pbounds, **{("variables-{}-{}".format(agentType, key)): append[key] for key in append}}
 
+    runCmd("sbt clean compile")
+    for stepSize in stepSizes:
+        runCmd('sbt "run generate {} {} {} {}"'.format(json_original, sampleSize, nSteps, stepSize))
+        for entry in range(sampleSize):
             optimizer = BayesianOptimization(None, pbounds, 10)
             utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.0)
-            optimization_iters = 20
             for i in range(optimization_iters):
                 print("stepSize:", stepSize, "entry:", entry, "iteration:", i)
                 next_point = optimizer.suggest(utility)
