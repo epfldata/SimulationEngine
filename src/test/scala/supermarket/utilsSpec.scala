a0cc21109ac5011280d2713980164a8eb902bf8a
ZiluTian <zilu.tian@epfl.ch>
2020-01-01 20:50:48 +0100

    Update test file for utils, items, and categories

---
+++ src/test/scala/supermarket/utilsSpec.scala
@@ -0,0 +1,27 @@
+package supermarket
+import meta.example.supermarket.{categoryAmount, utils}
+import org.scalatest._
+
+class utilsSpec extends FlatSpec with Matchers{
+
+  "ccArgToList" should "convert the parameter list of a case class to a list" in {
+    val foo: categoryAmount = categoryAmount(1, 2, 3, 4, 5)
+    utils.ccArgToList(foo) should be
+    (List(("Vegetable", 1.0), ("Meat", 2.0), ("Snack", 3.0), ("Grain", 4.0), ("Dairy", 5.0)))
+  }
+
+  "toInt" should "convert a Boolean to int. Return 1 if true 0 otherwise" in {
+    utils.toInt(true) should be (1)
+    utils.toInt(false) should be (0)
+  }
+
+  "to2Dec" should "keep 2 decimal places of a floating point val" in {
+    utils.to2Dec(1.23456) should be (1.23)
+  }
+
+  "divCeil" should "Return the ceil of two integer division" in {
+    utils.divCeil(4, 2) should be (2)
+    utils.divCeil(4, 3) should be (2)
+    utils.divCeil(1, 5) should be (1)
+  }
+}
