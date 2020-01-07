07fcf61bf0479d67c08c2ec62572cc4ebe8f1cc4
ZiluTian <zilu.tian@epfl.ch>
2020-01-07 20:53:06 +0100

    Use list range in place of while loop

--- src/main/scala/meta/example/supermarket/people/Customer1.scala
+++ src/main/scala/meta/example/supermarket/people/Customer1.scala
@@ -9,22 +9,17 @@ import squid.quasi.lift
 class Customer1 extends People with BiWeekly with MealPlanV1_1 with TargetShopper {
 
   def main(): Unit = {
-    var ctr: Int = 0
+
     while(true) {
       SpecialInstructions.handleMessages()
-      customerInfo
       buyListedItems(shoppingList.targetItems)
       buyRandItems(shoppingList.randItems)
-//      (1 to 3).foreach(_ =>{
-//        consumeFood(mealPlan)
-//        SpecialInstructions.waitTurns(1)
-//      })
-      ctr = 0
-      while(ctr < frequency){
+      List.range(0, frequency).foreach(_ => {
         consumeFood(mealPlan)
+        consumeFood
+        customerInfo
         SpecialInstructions.waitTurns(1)
-        ctr = ctr + 1
-      }
+      })
     }
   }
 }
