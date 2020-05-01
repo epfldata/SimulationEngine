0742b3868595eda9b2ebe6b9588c236dde1cc129
ZiluTian <zilu.tian@epfl.ch>
2020-05-01 14:10:13 +0200

    Include testing for async calls in SSO. Doesn't work for now

--- src/main/scala/meta/example/stateless_server/RandomNumberServer.scala
+++ src/main/scala/meta/example/stateless_server/RandomNumberServer.scala
@@ -11,6 +11,13 @@ class RandomNumberServer extends Actor {
   def getNumber(): Int = {
     nextInt(1000)
   }
+
+  def getDelayedNumber(): Int = {
+    waitTurns(1)
+    nextInt(50)
+    // println(nextInt(50))
+  }
+
   def main(): Unit = {
     while (true) {
       waitTurns(1)
