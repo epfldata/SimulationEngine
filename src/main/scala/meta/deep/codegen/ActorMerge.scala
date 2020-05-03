06228fd0c4ccda07271aafe3e2cb64a60ed44082
ZiluTian <zilu.tian@epfl.ch>
2020-05-03 15:19:52 +0200

    The merged actor should have one mailbox, rather than two

--- src/main/scala/meta/deep/codegen/ActorMerge.scala
+++ src/main/scala/meta/deep/codegen/ActorMerge.scala
@@ -94,8 +94,8 @@ class ActorMerge(mergeData: List[(String, String)])
         a1.actorTypes ::: a2.actorTypes,
         a1.positionStack ::: a2.positionStack,
         a1.returnValue,
-        a1.responseMessage ::: a2.responseMessage,
-        a1.responseMessagess ::: a2.responseMessagess,
+        a1.responseMessage,
+        a1.responseMessagess,
       ) :: newActorGraphs
     })
 
