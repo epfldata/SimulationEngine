5fceb8b758ed356b151a24f91ab614ece94c0fdd
ZiluTian <zilu.tian@epfl.ch>
2020-12-14 23:30:29 +0100

    Make Simulation a class for extendability and rewrite for modularity

--- src/main/scala/meta/simulation/Simulation.scala
+++ src/main/scala/meta/simulation/Simulation.scala
@@ -2,65 +2,84 @@ package generated.simulation
 
 import meta.deep.runtime.{Actor, Monitor}
 import meta.deep.runtime.Actor._
+
+import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
 object Simulation {
+  val eachIteration: ListBuffer[()=> Unit] = new ListBuffer()
+}
 
-  def run(config: SimulationConfig): SimulationSnapshot = {
+class Simulation(config: SimulationConfig) {
+  import Simulation._
+  var actors: List[Actor] = config.actors
+  var currentTurn: Int = config.startTurn
+  var currentTime: Double = config.startTime
+  val totalTurn: Int = config.totalTurn
+  val totalTime: Double = config.totalTime
 
-    var actors: List[Actor] = config.actors
-    var currentTurn: Int = config.startTurn
-    var currentTime: Double = config.startTime
-    val totalTurn: Int = config.totalTurn
-    val totalTime: Double = config.totalTime
-    val monitorEnabled: Boolean = config.monitorEnabled
+  def collect(currentTurn: Int): Unit = {
+    newActors.map(i => i.currentTurn = currentTurn)
+    actors = actors ::: newActors.toList
+    newActors.clear()
+  }
 
-    def collect(currentTurn: Int): Unit = {
-      newActors.map(i => i.currentTurn = currentTurn)
-      actors = actors ::: newActors.toList
-      newActors.clear()
-    }
+  def proceed(): Unit = {
+    proceedGroups()
+    currentTurn += proceedLabel("turn").asInstanceOf[Int]
+    currentTime += proceedLabel("time")
 
-    def proceed(): Unit = {
-      proceedGroups()
-      currentTurn += proceedLabel("turn").asInstanceOf[Int]
-      currentTime += proceedLabel("time")
+    // update the turn counter for Sims
+    actors.foreach(i => {
+      i.currentTime = currentTime
+      i.currentTurn = currentTurn
+    })
+  }
 
-      // update the turn counter for Sims
-      actors.map(i => {
-        i.currentTime = currentTime
-        i.currentTurn = currentTurn
-      })
-    }
+  // register events
+  def addEvent(e: => ()=> Unit): Unit = {
+    eachIteration.append(e)
+  }
 
+  // Can be overridden in an inherited class. Same for scheduleEvents
+  def init(): Unit = {
     initLabelVals()
+    scheduleEvents()
+  }
 
-    val start = System.nanoTime()
-    println("Monitor is enabled: " + monitorEnabled)
-    while (currentTurn <= totalTurn && currentTime <= totalTime) {
-      util.displayTime(currentTurn, currentTime)
-      collect(currentTurn)
-      waitLabels("time") = actors.length
+  private var lastTurn: Int = -1
 
-      val mx = actors.flatMap(_.getSendMessages).groupBy(_.receiverId)
-
-      actors = actors.map { a =>
-        {
-          a.cleanSendMessage
-            .addInterrupts(currentTime)
-            .addReceiveMessages(Random.shuffle(mx.getOrElse(a.id, List())))
-            .run_until(currentTurn)
-        }
+  def scheduleEvents(): Unit = {
+    addEvent(() => {
+      if (lastTurn != currentTurn) {
+        println(util.displayTime(currentTurn, currentTime))
+        lastTurn = currentTurn
       }
+    })
+    addEvent(() => collect(currentTurn))
+    addEvent(() => waitLabels("time") = actors.length)
+    addEvent(() => {
+      val mx = actors.flatMap(_.getSendMessages).groupBy(_.receiverId)
+      actors = actors.map { a =>
+      {
+        a.cleanSendMessage
+//                  .addInterrupts(currentTime)
+          .addReceiveMessages(Random.shuffle(mx.getOrElse(a.id, List())))
+          .run_until(currentTurn)
+      }}})
+    addEvent(() => proceed())
+  }
 
-      if (monitorEnabled) Monitor.eachIteration(()=>())
-      proceed()
+  def run(): SimulationSnapshot = {
+    init()
+    val start = System.nanoTime()
+    while (currentTurn <= totalTurn && currentTime <= totalTime) {
+      eachIteration.foreach(m => m())
     }
 
     val end = System.nanoTime()
     val consumed = end - start
-    if (monitorEnabled) Monitor.onCompletion()
     println("Time consumed", consumed)
-    SimulationSnapshot(actors, currentTurn, currentTime, monitorEnabled)
+    SimulationSnapshot(actors, currentTurn, currentTime)
   }
 }
