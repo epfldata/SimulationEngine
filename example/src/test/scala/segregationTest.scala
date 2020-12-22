ba95dfee6dbd74c0b9bd517e6a08ca86550fae59
ZiluTian <zilu.tian@epfl.ch>
2020-12-22 10:34:23 +0100

    Update the source segregation test

--- example/src/test/scala/segregationTest.scala
+++ example/src/test/scala/segregationTest.scala
@@ -1,6 +1,6 @@
 import breeze.numerics.abs
 import org.scalatest.{FlatSpec, Matchers}
-import meta.example.segregation.Torus2D
+import example.segregation.Torus2D
 
 class segregationTest extends FlatSpec with Matchers {
 
@@ -18,26 +18,5 @@ class segregationTest extends FlatSpec with Matchers {
 //    torusTest(5, 3).neighborCells(7, 2) shouldBe Set(2, 6, 7, 8, 12, 1, 5, 11, 13, 3, 9)
       torusTest(w1, h1).neighborCells(0, 1) shouldBe Set(1, 2, 3, 4, 5, 6, 7, 8)
       torusTest(w1, h1).neighborCells(4, 1) shouldBe Set(0, 1, 2, 3, 5, 6, 7, 8)
-  }
-
-  import _root_.generated.simulation.{SimulationConfig => Config, _}
-  import generated.meta.example.segregation
-  import meta.example.recordLogEvents
-  import ch.qos.logback.classic.Level._
-
-  "The similarity level of the segregation model" should "converge to around 0.85" in {
-    val totalTurns: Int = 70
-    val last10 = recordLogEvents.record({
-      Simulation.run(Config(segregation.InitData.initActors, 0, totalTurns, 0, 0, false))
-    }).filter(e => e.getLevel().equals(INFO)).map(i => i.getFormattedMessage.stripMargin.toDouble).drop(totalTurns-10)
-    assert(abs(last10.sum / last10.length - 0.85) < 0.03)
-  }
-
-  "The similarity level of the segregation model" should "converge to around 0.85, Spark" in {
-    val totalTurns: Int = 70
-    val last10 = recordLogEvents.record({
-      SimulationSpark.run(Config(segregation.InitData.initActors, 0, totalTurns, 0, 0, false))
-    }).filter(e => e.getLevel().equals(INFO)).map(i => i.getFormattedMessage.stripMargin.toDouble).drop(totalTurns-10)
-    assert(abs(last10.sum / last10.length - 0.85) < 0.03)
   }
 }
