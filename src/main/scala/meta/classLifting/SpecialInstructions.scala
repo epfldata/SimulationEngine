50cc1b0a6241ca587d3a4703a4969c188b059b82
ZiluTian <zilu.tian@epfl.ch>
2020-12-23 21:04:39 +0100

    Bug fix of the previous commit

--- src/main/scala/meta/classLifting/SpecialInstructions.scala
+++ src/main/scala/meta/classLifting/SpecialInstructions.scala
@@ -2,6 +2,8 @@ package meta.classLifting
 
 
 object SpecialInstructions {
+  import meta.deep.runtime.Future
+  import Broadcast._
 
   /** makes the actor wait for a number of turns
     *
@@ -11,9 +13,20 @@ object SpecialInstructions {
   // Create a synchronization label for the selected Sims.
   def waitLabel(label: String, waitValue: Double): Unit = ???
 
+  def asyncMessage[T](message: (()=>T)): Option[Future[T]] = ???
+
+  def broadcast[A, T](recipients: List[A], mtd: A=>T, mode: broadcastMode): Option[List[Future[T]]] = ???
+
   // Handle incoming messages
   def handleMessages(): Unit = ???
 
   // Interrupt for time
   def interrupt(interval: Double, method: (()=>Any)): Unit = ???
+}
+
+object Broadcast {
+  sealed trait broadcastMode
+  case object waitUntilRecipientsReceived extends broadcastMode
+  case object waitUntilRecipientsReplied extends broadcastMode
+  case object noWait extends broadcastMode
 }
