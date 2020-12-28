536aaa27354623722b1454b0e09890cb41d5f0cb
ZiluTian <zilu.tian@epfl.ch>
2020-12-28 17:05:42 +0100

    Clean up special instructions def

--- src/main/scala/meta/classLifting/SpecialInstructions.scala
+++ src/main/scala/meta/classLifting/SpecialInstructions.scala
@@ -1,9 +1,7 @@
 package meta.classLifting
 
-
 object SpecialInstructions {
   import meta.runtime.Future
-  import Broadcast._
 
   /** makes the actor wait for a number of turns
     *
@@ -15,18 +13,9 @@ object SpecialInstructions {
 
   def asyncMessage[T](message: (()=>T)): Option[Future[T]] = ???
 
-  def broadcast[A, T](recipients: List[A], mtd: A=>T, mode: broadcastMode): Option[List[Future[T]]] = ???
-
   // Handle incoming messages
   def handleMessages(): Unit = ???
 
   // Interrupt for time
   def interrupt(interval: Double, method: (()=>Any)): Unit = ???
-}
-
-object Broadcast {
-  sealed trait broadcastMode
-  case object waitUntilRecipientsReceived extends broadcastMode
-  case object waitUntilRecipientsReplied extends broadcastMode
-  case object noWait extends broadcastMode
 }
