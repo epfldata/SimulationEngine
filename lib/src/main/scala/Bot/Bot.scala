af50cbbd7dc5cba67b70f20c81575b7476d214e9
ZiluTian <zilu.tian@epfl.ch>
2020-12-29 11:54:19 +0100

    Remove Future from Bot's package obj to avoid the type aliasing

--- lib/src/main/scala/Bot/Bot.scala
+++ lib/src/main/scala/Bot/Bot.scala
@@ -3,7 +3,5 @@ package lib
 package object Bot {
   type Actor = meta.runtime.Actor
   val Actor = meta.runtime.Actor
-  type Future[T] = meta.runtime.Future[T]
-  val Future = meta.runtime.Future
   val SimRuntime = meta.runtime.SimRuntime
 }
