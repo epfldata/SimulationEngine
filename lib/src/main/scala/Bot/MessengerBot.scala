12df997c719fd74693955f2f8b9635497a21c747
ZiluTian <zilu.tian@epfl.ch>
2020-12-29 11:57:47 +0100

    Add another method to MessengerBot and update changes related to async_messages

--- lib/src/main/scala/Bot/MessengerBot.scala
+++ lib/src/main/scala/Bot/MessengerBot.scala
@@ -1,19 +1,30 @@
 package lib.Bot
 
 import meta.classLifting.SpecialInstructions._
+import meta.runtime.Future
 import squid.quasi.lift
 
 @lift
-class MessengerBot(val sender: Actor) extends Actor {
+class MessengerBot() extends Actor {
 
   var deleted: Boolean = false
 
-  def waitUntilAllReceived(future_objs: List[Option[Future[Unit]]]): Unit = {
-    while (!(future_objs.nonEmpty && future_objs.forall(x => sender.isCompleted(x.get)))) {
+  def waitUntilAllReceive(future_objs: List[Option[Future[Any]]]): Unit = {
+    while (!(future_objs.nonEmpty && future_objs.forall(x => SimRuntime.isCompleted(x.get)))) {
       waitLabel("turn",1)
     }
-    future_objs.foreach(o => sender.clearFutureObj(o.get))
+    future_objs.foreach(o => SimRuntime.clearFutureObj(o.get))
     deleted = true
+  }
+
+  def waitUntilAllReply(future_objs: List[Option[Future[Any]]]): List[Any] = {
+    while (!(future_objs.nonEmpty && future_objs.forall(x => SimRuntime.isCompleted(x.get)))) {
+      waitLabel("turn",1)
+    }
+    val ans: List[Any] = future_objs.map(o => SimRuntime.getFutureValue[Any](o.get))
+    future_objs.foreach(o => SimRuntime.clearFutureObj(o.get))
+    deleted = true
+    ans
   }
 
   def main(): Unit = {
