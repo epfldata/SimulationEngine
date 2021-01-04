cf012027884a621a2426b8e3fb5c3a1618200a15
ZiluTian <zilu.tian@epfl.ch>
2021-01-04 15:44:37 +0100

    Add comments for Config and Snapshot

--- src/main/scala/meta/runtime/simulation/SimulationConfig.scala
+++ src/main/scala/meta/runtime/simulation/SimulationConfig.scala
@@ -1,8 +1,17 @@
 package meta.runtime
 package simulation
 
+/**
+  * Defines the configuration for a simulation run
+  * Simulation terminates when totalTurn or totalTime is reached (or both)
+  * @param actors the Sims (agents) to run simulation on. Default $object program$ InitData.initActors. Please reference scripts in /generated/src/test/
+  * @param startTurn define what the first turn of the simulation is
+  * @param totalTurn define for how many turns the simulation continues
+  * @param startTime define the initial time of the simulation
+  * @param totalTime define the total time of the simulation
+  */
 case class SimulationConfig(var actors: List[Actor],
                             var startTurn: Int = 0,
-                            val totalTurn: Int = 100,
+                            val totalTurn: Int = 40,
                             var startTime: Double = 0,
                             val totalTime: Double = 10)
--- src/main/scala/meta/runtime/simulation/SimulationSnapshot.scala
+++ src/main/scala/meta/runtime/simulation/SimulationSnapshot.scala
@@ -1,6 +1,14 @@
 package meta.runtime
 package simulation
 
+/**
+  * Capture the state of the simulation.
+  * run() returns a snapshot object, where milliTime records the duration of the run
+  * @param actors the Sims at the current state
+  * @param currentTurn the turn when the snapshot is taken
+  * @param currentTime the time when the snapshot is taken
+  * @param milliTime the execution time in ms that the snapshot covers
+  */
 case class SimulationSnapshot(var actors: List[Actor] = List(),
                               var currentTurn: Int = 0,
                               var currentTime: Double = 0,
