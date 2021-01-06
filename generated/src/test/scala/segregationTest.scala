72d136e8a4b2b816ba814ee255e6154f11766ade
ZiluTian <zilu.tian@epfl.ch>
2021-01-06 13:35:41 +0100

    Add draw timeseries

--- generated/src/test/scala/segregationTest.scala
+++ generated/src/test/scala/segregationTest.scala
@@ -1,25 +1,31 @@
 package generated.example
 package test
 
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.{Scene, Node}
+
+import scalafx.scene.chart.{NumberAxis, ScatterChart, XYChart}
+import scalafx.embed.swing.SwingFXUtils
+import java.io.File
+import javax.imageio.ImageIO
+
 class segregationTest extends FlatSpec {
 
   import segregation._
-  import ch.qos.logback.classic.Level._
 
   "The similarity level of the segregation model" should "converge to around 0.85" in {
-    val totalTurns: Int = 70
-    val Sim = new Simulation(new Config(InitData.initActors, 0, totalTurns, 0, 0))
-    val last10 = recordLogEvents.record({
-      Sim.run()
-    }).filter(e => e.getLevel().equals(INFO)).map(i => i.getFormattedMessage.stripMargin.toDouble).drop(totalTurns-10)
-    assert((last10.sum / last10.length - 0.85).abs < 0.03)
-  }
+    val totalTurns: Int = 100
+    val config: Config = new Config(InitData.initActors, 0, totalTurns, 0, 0)
+    val res = new Default(config).run()
+//    val res = new SimulationSpark(config).run()
 
-//  "The similarity level of the segregation model" should "converge to around 0.85, Spark" in {
-//    val totalTurns: Int = 70
-//    val last10 = recordLogEvents.record({
-//      SimulationSpark.run(new Config(segregation.InitData.initActors, 0, totalTurns, 0, 0))
-//    }).filter(e => e.getLevel().equals(INFO)).map(i => i.getFormattedMessage.stripMargin.toDouble).drop(totalTurns-10)
-//    assert((last10.sum / last10.length - 0.85).abs < 0.03)
-//  }
+    val series: List[(Double, Double)] = res.actors
+      .filter(a => a.isInstanceOf[LoggerBotTimeseries])
+      .map(x => x.asInstanceOf[LoggerBotTimeseries].timeseries)
+      .head
+
+    DrawTimeseries("Schelling's segregation example", 500, 500, series)
+  }
 }
