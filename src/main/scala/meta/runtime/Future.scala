6538ca66f7c2d142d9c9423117f95e49f01b6ae4
ZiluTian <zilu.tian@epfl.ch>
2021-03-24 12:18:07 +0100

    Update Future's comment

--- src/main/scala/meta/runtime/Future.scala
+++ src/main/scala/meta/runtime/Future.scala
@@ -9,15 +9,17 @@ package meta.runtime
 case class Future[+T](val id: String, 
                       val value: Option[T] = None){
 
+  // The call back (message handler) of an asynchronous message will call setValue to update the value of the asynchronous call                       
   def setValue[U >: T](y: U): Future[U] ={
     Future(id, Some(y))
   }
 
+  // Check whether the future object has received its value, allow repeated checking  
   def isCompleted: Boolean = {
     SimRuntime.isCompleted(this)
   }
 
-  // Single-entry 
+  // Return the value of the future object. If None, then hasn't completed; otherwise, return Some(value). Once the value is ready, can only call popValue at most once, which will remove the future object from the internal async_message map; subsequent calls will return None   
   def popValue: Option[T] = {
     SimRuntime.popFutureValue(this) 
   }
