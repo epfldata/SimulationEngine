ea257acc1ae438529df2fbe83287055a2acb56db
ZiluTian <zilu.tian@epfl.ch>
2021-03-26 15:13:03 +0100

    Update Container.scala
    Define reflection methods in the container agents as well

--- src/main/scala/meta/runtime/Container.scala
+++ src/main/scala/meta/runtime/Container.scala
@@ -1,11 +1,13 @@
 package meta.runtime
 
 /**
- * A container agent holds a collection of agents 
+ * A container agent holds a collection of agents. 
+ * The internal messages among the agents are non-blocking. 
  */ 
 class Container extends Actor {
-  var containedAgents: List[Actor] = List()
+  var position: Int = 0 
 
+  var containedAgents: List[Actor] = List()
   var internalMessages: List[Message] = List()
 
   // When add agents to a container, also update the proxyIds 
@@ -21,40 +23,58 @@ class Container extends Actor {
   // proxyIds initialized after addAgents. Therefore init again inside run. An optimization (if large number of agents don't send internal messages), not required
   var unblockAgents: Set[Actor.AgentId] = Set()
 
+  val commands: List[() => Unit] = List(
+    () => {
+        unblockAgents = proxyIds.toSet 
+        cleanSendMessage
+
+        containedAgents = containedAgents.map(a => {
+        a.cleanSendMessage 
+        .addReceiveMessages(mx.getOrElse(a.id, List()))
+        .run()
+        })
+
+        do {
+        // get all the messages  
+        messageBuffer = containedAgents.flatMap(_.getSendMessages)
+        // remove the sent messages from the agents 
+        containedAgents = containedAgents.map(_.cleanSendMessage)
+        // filter out the internal messages 
+        internalMessages = messageBuffer.filter(x => proxyIds.contains(x.receiverId))
+        // append the external messages to the outgoing mailbox  
+        sendMessages = messageBuffer.diff(internalMessages) ::: sendMessages 
+        // update the unblockAgent indexes to selectively deliver the internal blocking messages
+        unblockAgents = internalMessages.flatMap(x => List(x.receiverId)).toSet  
+        // update the messages to deliver for the selected unblocking agents
+        mx = internalMessages.groupBy(_.receiverId)
+        
+        containedAgents = containedAgents.map(a => if (unblockAgents.contains(a.id)) {
+            val currentPtr: Int = a.gotoHandleMessage
+            val b: Actor = a.addReceiveMessages(mx.getOrElse(a.id, List())).run()
+            b.setInstructionPointer(currentPtr)
+            b 
+        } else a)
+
+        } while (internalMessages.nonEmpty)
+    }
+  )
+
   // Assume each wait in main follows a handleMessage 
   override def run(): Actor = {
-    unblockAgents = proxyIds.toSet 
-    cleanSendMessage
-
-    containedAgents = containedAgents.map(a => {
-      a.cleanSendMessage 
-      .addReceiveMessages(mx.getOrElse(a.id, List()))
-      .run()
-    })
-
-    do {
-      // get all the messages  
-      messageBuffer = containedAgents.flatMap(_.getSendMessages)
-      // remove the sent messages from the agents 
-      containedAgents = containedAgents.map(_.cleanSendMessage)
-      // filter out the internal messages 
-      internalMessages = messageBuffer.filter(x => proxyIds.contains(x.receiverId))
-      // append the external messages to the outgoing mailbox  
-      sendMessages = messageBuffer.diff(internalMessages) ::: sendMessages 
-      // update the unblockAgent indexes to selectively deliver the internal blocking messages
-      unblockAgents = internalMessages.flatMap(x => List(x.receiverId)).toSet  
-      // update the messages to deliver for the selected unblocking agents
-      mx = internalMessages.groupBy(_.receiverId)
-      
-      containedAgents = containedAgents.map(a => if (unblockAgents.contains(a.id)) {
-        val currentPtr: Int = a.gotoHandleMessage
-        val b: Actor = a.addReceiveMessages(mx.getOrElse(a.id, List())).run()
-        b.setInstructionPointer(currentPtr)
-        b 
-      } else a)
-
-    } while (internalMessages.nonEmpty)
-
+    commands(position)()
     this 
+  }
+
+  // Implement the reflection API  
+  override def gotoHandleMessage: Int = 0 
+
+  override def getInstructionPointer: Int = 0
+
+  override def setInstructionPointer(new_ir: Int): Int = {
+    if (new_ir!=0) {
+        throw new Exception(s"Invalid address pointer ${new_ir} for agent ${id}")
+    } else {
+        0
+    }
   }
 }
