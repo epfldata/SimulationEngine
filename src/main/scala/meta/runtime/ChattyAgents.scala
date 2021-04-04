52e667e36b86f9dfee9ee6c6546c83e1b9f30afc
ZiluTian <zilu.tian@epfl.ch>
2021-04-04 15:12:51 +0200

    Remove the mergeCandidates logic from ChattyAgents
    Duplicate in the runtime

--- src/main/scala/meta/runtime/ChattyAgents.scala
+++ src/main/scala/meta/runtime/ChattyAgents.scala
@@ -7,13 +7,20 @@ import Actor.AgentId
 class ChattyAgents(var mergeThreshold: Int) {
   private val profile = new MessageProfiling()
 
-  private var mergeCandidates: ListBuffer[Set[AgentId]] = new ListBuffer()
+  private var pairCandidates: ListBuffer[List[AgentId]] = new ListBuffer()
 
   def recordMessage(sim1: AgentId, sim2: AgentId): Unit = {
     profile.recordMessage(sim1, sim2)
     if (profile.getSketch(sim1, sim2) >= mergeThreshold) {
-      addMergeCandidates(sim1, sim2)
+      pairCandidates += List(sim1, sim2)
+      profile.clearSketch(sim1, sim2)
     }
+  }
+
+  def recordMessage(messages: List[Message]): Unit = {
+    messages.foreach(m => {
+      recordMessage(m.senderId, m.receiverId)
+    })
   }
 
   // the threshold beyond which we kick in the merging mechanism
@@ -25,37 +32,11 @@ class ChattyAgents(var mergeThreshold: Int) {
       mergeThreshold
   }
 
-  def getMergeCandidates: List[Set[AgentId]] = {
-      mergeCandidates.toList
+  def getPairedMergeCandidates: List[List[AgentId]] = {
+      pairCandidates.toList
   }
 
   def clearMergeCandidates(): Unit = {
-    mergeCandidates.clear()
-  }
-
-  def addMergeCandidates(sim1: AgentId, sim2: AgentId): Unit = {
-    var existingPlaces: List[Int] = List()
-
-    mergeCandidates.zipWithIndex.foreach(x => {
-      if (x._1.contains(sim1) || x._1.contains(sim2)) {
-        existingPlaces = x._2 :: existingPlaces
-      }
-    })
-
-    // head < tail.head
-    existingPlaces = existingPlaces.sorted
-    // 3 cases: 
-    // If sim1 and sim2 are both new to mergeCandidates, create a new set for them 
-    // If only one is new, then place both sims into the original array 
-    // If none is new, merge the two original array 
-    existingPlaces match {
-      case Nil => 
-        mergeCandidates += Set(sim1, sim2)
-      case head :: Nil =>
-        mergeCandidates(head) = mergeCandidates(head).union(Set(sim1, sim2))
-      case head :: tail => 
-        val foo = mergeCandidates.remove(tail.head)
-        mergeCandidates(head) = mergeCandidates(head).union(foo)
-    }
+    pairCandidates.clear()
   }
 }
