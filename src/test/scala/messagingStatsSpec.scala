717dee8a9f9a6aae4967105279703793f4085f83
ZiluTian <zilu.tian@epfl.ch>
2021-04-04 15:13:04 +0200

    Update the test to reflect API changes

--- src/test/scala/messagingStatsSpec.scala
+++ src/test/scala/messagingStatsSpec.scala
@@ -14,15 +14,9 @@ class messagingStatsSpec extends FlatSpec {
         (1 to 3).foreach(x => {
             recordHotMessageTest.recordMessage(2, 3)
         })
-        assert(recordHotMessageTest.getMergeCandidates == List(Set(1, 0), Set(2, 3)))
+        assert(recordHotMessageTest.getPairedMergeCandidates == List(List(1, 0), List(2, 3)))
 
         recordHotMessageTest.recordMessage(1, 2)
-        assert(recordHotMessageTest.getMergeCandidates == List(Set(0, 1), Set(2, 3)))
-
-        (1 to 3).foreach(x => {
-            recordHotMessageTest.recordMessage(2, 1)
-        })
-
-        assert(recordHotMessageTest.getMergeCandidates == List(Set(0, 1, 2, 3)))
+        assert(recordHotMessageTest.getPairedMergeCandidates == List(List(1, 0), List(2, 3), List(1, 2)))
     }
 }
