3ee7f6cb50211df2c3fd98748e0cf695fb5aa903
ZiluTian <zilu.tian@epfl.ch>
2021-04-25 16:11:26 +0200

    Update method info with toString for method generation, declaration, and those of wrapper methods

--- src/main/scala/meta/deep/member/MethodInfo.scala
+++ src/main/scala/meta/deep/member/MethodInfo.scala
@@ -2,6 +2,7 @@ package meta.deep.member
 
 import meta.deep.IR
 import meta.deep.IR.Predef._
+import breeze.macros.expand
 
 /** contains all info about a method except the body of the method
   * used body methods to gather the needed data when calling this method
@@ -18,6 +19,75 @@ class MethodInfo[A0](val symbol: String,
                     val body: OpenCode[A0])(implicit val A: CodeType[A0]) {
   def replica(newSym: String): MethodInfo[A] = {
     new MethodInfo[A](newSym, this.tparams, this.vparams, this.body)(A)
+  }
+
+  val mtdName: String = symbol.split("\\.").tail.mkString(".")
+
+  // symbol name, type
+  private var bodyStr: String = body.showScala
+
+  private val argSyms: Option[List[(String, String)]]
+  =     
+    vparams match {
+      case Nil => None
+      case _ => {
+        Some(vparams.head.map(x => {
+          println(f"Arg syms: ${x.rep.show}")
+          val recoverName = x.rep.show.split("@").head
+          bodyStr = bodyStr.replaceAll(x.rep.show, recoverName)
+          (recoverName, x.Typ.rep.toString())
+        }))
+      }
+    }
+
+  // Bind the list of Any in argss to corresponding parameters  
+  def toWrapperDeclaration(): String = {
+    val argBinds: String = {
+      argSyms match {
+        case None => 
+          f"${mtdName}"
+        case Some(x) => 
+          f"""
+          ${x.zipWithIndex.map(p => "val " + p._1._1  + ": " + p._1._2 + " = " + "args(" + p._2 + ").asInstanceOf[" + p._1._2 + "]").mkString("\n    ")}
+          ${mtdName}(${x.map(p => p._1).mkString(",")})
+          """
+      }
+    }
+
+    f"""
+    def wrapper_${mtdName}(args: List[Any]): ${A.rep.toString} = {
+      ${argBinds}
+    }
+    """
+  }
+
+  def toDeclaration(): String = {
+    argSyms match {
+      case None => 
+      f"""
+      def ${mtdName}: ${A.rep.toString} = 
+          ${bodyStr}
+      
+      """
+      case Some(x) => 
+      f"""
+      def ${mtdName}(${x.map(p => p._1 + ": " + p._2).mkString(",")}): ${A.rep.toString} = 
+          ${bodyStr}
+      """
+    }
+  }
+
+  def toWrapperInvocation(): String = {
+    f"""wrapper_${mtdName}(args)"""
+  }
+
+  def toInvocation(): String = {
+    argSyms match {
+      case None => 
+        f"${mtdName}"
+      case Some(x) => 
+        f"""${mtdName}(${x.map(p => p._1).mkString(",")})"""
+    }
   }
 
   type A = A0 
