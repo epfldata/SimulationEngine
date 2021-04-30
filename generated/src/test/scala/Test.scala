29af1fdb707f3e7272251a265420d7f26059e164
ZiluTian <zilu.tian@epfl.ch>
2021-04-30 15:06:40 +0200

    Update Test.scala

--- generated/src/test/scala/Test.scala
+++ generated/src/test/scala/Test.scala
@@ -1,9 +1,6 @@
 package generated.example
 
 package object test {
-  type Default = meta.runtime.simulation.Default
-  type SimulationSpark = meta.runtime.simulation.SimulationSpark
-  type Config = meta.runtime.simulation.SimulationConfig
   val util = meta.runtime.simulation.util
   type FlatSpec = org.scalatest.FlatSpec
 }
