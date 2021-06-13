886bfb7169c32573faace2d26d13f813dcaac5b9
ZiluTian <zilu.tian@epfl.ch>
2021-06-13 22:16:03 +0200

    Update the test script for genExample

--- generated/src/test/scala/generatedExampleSpec.scala
+++ generated/src/test/scala/generatedExampleSpec.scala
@@ -3,15 +3,14 @@ package generated.example.test
 import meta.API._
 
 class generatedExamples extends org.scalatest.FlatSpec {
-    "Compiled epidemic example with base messaging layer" should "run" in {
+    "Compiled epidemic example with Akka containers" should "run" in {
         val agents = generated.example.epidemic.InitData(100)
 
         val c = new SimulationConfig(agents, totalTurn = 10)
 
         // 10 containers
         val containerConfig = c.staticPartition(10)
-        // StartSimulation[AkkaMessagingLayer.type](containerConfig)
-        StartSimulation[BaseMessagingLayer.type](containerConfig)
+        val results = StartSimulation[AkkaMessagingLayer.type](containerConfig)
     }
 
     "Compiled game of life example with Akka messaging layer" should "run" in {
@@ -19,33 +18,34 @@ class generatedExamples extends org.scalatest.FlatSpec {
 
         val c = new SimulationConfig(agents, totalTurn = 10)
 
-        StartSimulation[AkkaMessagingLayer.type](c)
+        val results = StartSimulation[BaseMessagingLayer.type](c)
     }
 
-    "Compiled wator example with 10 containers over Akka" should "run" in {
+    "Compiled wator example with 10 containers over base" should "run" in {
         val agents = generated.example.cellularAutomata.wator.InitData(10, 10)
 
-        val c = new SimulationConfig(agents, totalTurn = 10)
-        c.staticPartition(10)
+        val c = new SimulationConfig(agents, totalTurn = 100)
+        val containerConfig = c.staticPartition(10)
 
-        StartSimulation[AkkaMessagingLayer.type](c)
+        val results = StartSimulation[BaseMessagingLayer.type](containerConfig)
+        // val results = StartSimulation[AkkaMessagingLayer.type](containerConfig)
     }
+
 
     "Compiled segregation example with 10 containers over base" should "run" in {
         val agents = generated.example.segregation.InitData(10, 10, 70)
 
         val c = new SimulationConfig(agents, totalTurn = 10)
-        c.staticPartition(10)
+        val containerConfig = c.staticPartition(10)
 
-        StartSimulation[BaseMessagingLayer.type](c)
+        val results = StartSimulation[BaseMessagingLayer.type](containerConfig)
     }
 
     "Compiled wealth distribution example over Akka" should "run" in {
         val agents = generated.example.sugarscape.wealthDistribution.InitData(10, 10)
 
-        val c = new SimulationConfig(agents, totalTurn = 10)
-        c.staticPartition(10)
+        val c = new SimulationConfig(agents, 10)
 
-        StartSimulation[AkkaMessagingLayer.type](c)
+        val results = StartSimulation[AkkaMessagingLayer.type](c)
     }
 }
