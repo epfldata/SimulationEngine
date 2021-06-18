2ca4d0a7f1397c4092bd0c92cce3e99bacb59df1
ZiluTian <zilu.tian@epfl.ch>
2021-06-18 18:30:31 +0200

    Update test scripts to reflect changes in container API

--- generated/src/test/scala/generatedExampleSpec.scala
+++ generated/src/test/scala/generatedExampleSpec.scala
@@ -1,6 +1,7 @@
 package generated.example.test
 
 import meta.API._
+import akka.io.Tcp
 
 class generatedExamples extends org.scalatest.FlatSpec {
     "Compiled epidemic example with Akka containers" should "run" in {
@@ -9,25 +10,26 @@ class generatedExamples extends org.scalatest.FlatSpec {
         val c = new SimulationConfig(agents, totalTurn = 10)
 
         // 10 containers
-        val containerConfig = c.staticPartition(10)
+        val containerConfig = c.staticPartition(10)(MessageCaching)
         val results = StartSimulation[AkkaMessagingLayer.type](containerConfig)
     }
 
     "Compiled game of life example with Akka messaging layer" should "run" in {
         val agents = generated.example.gameOfLife.InitData(10, 10)
 
-        val c = new SimulationConfig(agents, totalTurn = 10)
+        val c = new SimulationConfig(agents, 10)
+        val containerConfig = c.staticPartition(10)(DirectMethodCall)
 
-        val results = StartSimulation[BaseMessagingLayer.type](c)
+        val results = StartSimulation[BaseMessagingLayer.type](containerConfig)
     }
 
     "Compiled wator example with 10 containers over base" should "run" in {
         val agents = generated.example.cellularAutomata.wator.InitData(10, 10)
 
-        val c = new SimulationConfig(agents, totalTurn = 100)
-        val containerConfig = c.staticPartition(10)
+        val c = new SimulationConfig(agents, 10)
+        val containerConfig = c.staticPartition(10)(VanillaContainer)
 
-        val results = StartSimulation[BaseMessagingLayer.type](containerConfig)
+        val results = StartSimulation[BaseMessagingLayer.type](c)
         // val results = StartSimulation[AkkaMessagingLayer.type](containerConfig)
     }
 
@@ -35,10 +37,10 @@ class generatedExamples extends org.scalatest.FlatSpec {
     "Compiled segregation example with 10 containers over base" should "run" in {
         val agents = generated.example.segregation.InitData(10, 10, 70)
 
-        val c = new SimulationConfig(agents, totalTurn = 10)
-        val containerConfig = c.staticPartition(10)
+        val c = new SimulationConfig(agents, 10)
+        val containerConfig = c.staticPartition(10)(MessageCaching)
 
-        val results = StartSimulation[BaseMessagingLayer.type](containerConfig)
+        val results = StartSimulation[BaseMessagingLayer.type](c)
     }
 
     "Compiled wealth distribution example over Akka" should "run" in {
