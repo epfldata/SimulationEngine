3b276704133bdc7d77f2ae26d48968eaf7505373
ZiluTian <zilu.tian@epfl.ch>
2021-06-19 22:15:02 +0200

    Add test for new agents

---
+++ core/src/test/scala/generated/InitData.scala
@@ -0,0 +1,5 @@
+package generated.meta.test
+
+object InitData  {
+    def apply(): scala.`package`.List[meta.runtime.Actor] = scala.collection.immutable.List.apply[generated.meta.test.NewSim](new NewSim())
+}
---
+++ core/src/test/scala/newAgentAkkaTest.scala
@@ -0,0 +1,47 @@
+package meta.test
+
+import meta.classLifting.SpecialInstructions._
+import squid.quasi.lift
+import meta.deep.IR.TopLevel.ClassWithObject
+import meta.deep.IR
+import meta.runtime.{Actor}
+import meta.API._
+import org.scalatest.FlatSpec
+import meta.compile._
+
+@lift
+class NewSim() extends Actor {
+
+    def main(): Unit = {
+        while (true){
+            println("This is agent " + id)
+            val c = new NewSim()
+            println("Create new agent " + c.id)
+            waitLabel(Turn, 1)
+        }
+    }
+}
+
+class NewSimTest extends FlatSpec {
+    import meta.deep.IR.Predef._
+
+    "Generating new agents" should "compile" in {
+        val liftMyClass: ClassWithObject[NewSim] = NewSim.reflect(IR)
+        val liftedMain = custMacros.liftMethod {
+            def apply(): List[Actor] = {
+                List(new NewSim())
+            }
+        }
+
+        compileSims(List(liftMyClass), 
+            mainInit = Some(liftedMain), 
+            initPkgName = Some(this.getClass().getPackage().getName()),
+            destFolder = "core/src/test/scala/generated")
+    }
+
+    "The runtime" should "recognize the generated new agents" in {
+        val agents = generated.meta.test.InitData()
+        val c = new SimulationConfig(agents, 3)
+        StartSimulation[AkkaMessagingLayer.type](c)
+    }
+}
