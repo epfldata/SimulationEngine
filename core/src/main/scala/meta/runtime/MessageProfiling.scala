fb60d6440ed63fceca1812a12a4c10403ed39d56
ZiluTian <zilu.tian@epfl.ch>
2021-06-24 15:39:32 +0200

    Enhance MessageProfiling with some capacity of ChattyAgents

--- core/src/main/scala/meta/runtime/MessageProfiling.scala
+++ core/src/main/scala/meta/runtime/MessageProfiling.scala
@@ -3,41 +3,58 @@ package meta.runtime
 import scala.collection.mutable.{ListBuffer, Map => MutMap}
 import Actor.AgentId
 
-class MessageProfiling {
+class MessageProfiling() {
   private var sketch: MutMap[AgentId, MutMap[AgentId, Int]] = MutMap[AgentId, MutMap[AgentId, Int]]()
 
-  // def getSketch: Set[(AgentId, Map[AgentId, Int])] = sketch.map(x => (x._1, x._2.toMap)).toSet
-
-  def getSketch(sim1: AgentId, sim2: AgentId): Int = {
-    if (sim1 < sim2) {
-      sketch(sim1)(sim2)
-    } else {
-      sketch(sim2)(sim1)
-    }
+  // For a given agent sim1, returns the total number of communications it made
+  def getTotalMessages(sim1: AgentId): Int = {
+    sketch.foldLeft(0)((y, x) => {
+      if (x._1 == sim1) {
+        y + x._2.values.sum
+      } else {
+        y + x._2.getOrElse(sim1, 0)
+      }
+    })
   }
 
-  def clearSketch(sim1: AgentId, sim2: AgentId): Unit = {
-    if (sim1 < sim2) {
-      sketch(sim1) -= sim2 
-    } else {
-      sketch(sim2) -= sim1
-    }
+  def clearSketch(): Unit = {
+    sketch.clear()
   }
 
-  // todo: add overflow logic 
-  // record message one at a time
+  // Update the sketch
   def recordMessage(sim1: AgentId, sim2: AgentId): Unit = {
-
-    val simPair = List(sim1, sim2).sorted
-
-    if (sketch.get(simPair.head).isDefined) {
-        if (sketch(simPair.head).get(simPair.last).isDefined) {
-            sketch(simPair.head)(simPair.last) += 1
+    if (sketch.get(sim1).isDefined) {
+        if (sketch(sim1).get(sim2).isDefined) {
+            sketch(sim1)(sim2) += 1
         } else {
-            sketch(simPair.head).update(simPair.last, 1)
+            sketch(sim1).update(sim2, 1)
         }
     } else {
-        sketch.update(simPair.head, MutMap(simPair.last -> 1))
+        sketch.update(sim1, MutMap(sim2 -> 1))
     }
+  }
+
+  def recordMessage(messages: Seq[Message]): Unit = {
+    messages.foreach(m => {
+      if (m.senderId > m.receiverId){
+        recordMessage(m.receiverId, m.senderId)
+      } else {
+        recordMessage(m.senderId, m.receiverId)
+      }
+    })
+  }
+
+  def getMergeCandidates(mergeThreshold: Int): List[Tuple2[AgentId, AgentId]] = {
+    var mergingCandidates = List[Tuple2[AgentId, AgentId]]()
+
+    for (x <- sketch) {
+      for (y <- x._2) {
+        if (y._2 > mergeThreshold) {
+          mergingCandidates = (x._1, y._1) :: mergingCandidates 
+        }
+      }
+    }
+
+    mergingCandidates
   }
 }
