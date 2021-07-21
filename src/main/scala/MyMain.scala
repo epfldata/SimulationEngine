d5cb497216cdb166cfbd21d647411e61057b1fd3
christophkoch <christoph.koch@epfl.ch>
2021-07-21 17:49:06 +0200

    Removed more German stuff.

--- src/main/scala/MyMain.scala
+++ src/main/scala/MyMain.scala
@@ -14,7 +14,7 @@ object MainExample {
   val landlord        = new Source(Land,  20, 100000*100, s);
   //val cattle_farmer = new Source(Beef,   100,  26000*100, s);
   //val silo          = new Source(Wheat, 1000,   6668*100, s);
-  //val silo2         = new Trader(Whear, 100, s);
+  //val silo2         = new Trader(Wheat, 100, s);
   //val flour_trader  = new Trader(Flour, 50, s);
   val flour_buyer     = new Buyer(Flour, () => 40, s);
 
@@ -49,9 +49,9 @@ object TradingExample {
   simu.init(List(s, t, b));
   simu.run(4);
 /* After 4 steps we have
-(BalanceSheet(4000,4000,4000,0,0),ArrayBuffer(getreide -> 0@0))
-(BalanceSheet(50,50,50,0,0),ArrayBuffer(getreide -> 0@1000))
-(BalanceSheet(0,4050,0,-4050,0),ArrayBuffer(getreide -> 4@1012))
+(BalanceSheet(4000,4000,4000,0,0),ArrayBuffer(Wheat -> 0@0))
+(BalanceSheet(50,50,50,0,0),ArrayBuffer(Wheat -> 0@1000))
+(BalanceSheet(0,4050,0,-4050,0),ArrayBuffer(Wheat -> 4@1012))
 */
 }
 
