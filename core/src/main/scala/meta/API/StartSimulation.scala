8babfa12f24d2858e5d44b4f62cf34151fb33378
ZiluTian <zilu.tian@epfl.ch>
2021-11-10 16:58:01 +0100

    Add a bench method when starting the simulation

--- core/src/main/scala/meta/API/StartSimulation.scala
+++ core/src/main/scala/meta/API/StartSimulation.scala
@@ -1,7 +1,18 @@
 package meta.API
 
+import scala.collection.mutable.StringBuilder
+
 object StartSimulation {
   def apply[T](c: SimulationConfig)(implicit runner: SimsRunner[T]): SimulationSnapshot = {
         runner.run(c)
+    }
+
+  def bench[T](c: SimulationConfig)(w: StringBuilder)(implicit runner: SimsRunner[T]): Long = {
+      val totalTime = meta.runtime.simulation.util.bench {
+        runner.run(c)
+      }
+      w ++= c.toString
+      w ++= f" TotalTime:${totalTime}; TimePerIteration:${totalTime/c.totalTurn}\n"
+      totalTime
     }
 }
