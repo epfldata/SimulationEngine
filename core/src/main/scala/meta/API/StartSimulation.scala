659699d031a7db12a48073ff71cb676170dff14f
ZiluTian <zilu.tian@epfl.ch>
2021-11-10 17:42:28 +0100

    Change floating point perIt calculation in bench

--- core/src/main/scala/meta/API/StartSimulation.scala
+++ core/src/main/scala/meta/API/StartSimulation.scala
@@ -4,15 +4,16 @@ import scala.collection.mutable.StringBuilder
 
 object StartSimulation {
   def apply[T](c: SimulationConfig)(implicit runner: SimsRunner[T]): SimulationSnapshot = {
-        runner.run(c)
-    }
+      runner.run(c)
+  }
 
   def bench[T](c: SimulationConfig)(w: StringBuilder)(implicit runner: SimsRunner[T]): Long = {
-      val totalTime = meta.runtime.simulation.util.bench {
-        runner.run(c)
-      }
-      w ++= c.toString
-      w ++= f" TotalTime:${totalTime}; TimePerIteration:${totalTime/c.totalTurn}\n"
-      totalTime
+    println(f"Start simulation benchmark! ${c}")
+    val totalTime = meta.runtime.simulation.util.bench {
+      runner.run(c)
     }
+    w ++= c.toString
+    w ++= f" TotalTime:${totalTime}; TimePerIteration:${totalTime.toDouble/c.totalTurn}\n"
+    totalTime
+  }
 }
