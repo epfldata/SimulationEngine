5ce834ffbd851cf9a46ffb77b670e72c14ee4851
ZiluTian <zilu.tian@epfl.ch>
2021-12-15 20:33:29 +0100

    Add cust runner that removes per-round logging

--- core/src/test/scala/custRunnerTest.scala
+++ core/src/test/scala/custRunnerTest.scala
@@ -8,6 +8,7 @@ import meta.runtime._
 import meta.API._
 import org.scalatest.FlatSpec
 import meta.compile._
+import meta.runtime.simulation.Base
 
 // A test that shows how users can define their own runners
 class CustRunnerTest extends FlatSpec {
@@ -22,6 +23,37 @@ class CustRunnerTest extends FlatSpec {
                     println("A custom runner that does nothing!")
                 }
                 SimulationSnapshot(List(), List())
+            }
+        }
+        StartSimulation[BaseMessagingLayer.type](c)(custRunner)
+    }
+    
+    "Define a custom runner without logging round information" should "print only agent messages" in {
+        val agents = generated.meta.test.custAgent.InitData()
+        val c = new SimulationConfig(agents, 5)
+
+        val custRunner = new SimsRunner[BaseMessagingLayer.type] {
+            class noRoundBase(c: SimulationConfig) extends Base(c.actors, c.totalTurn, c.messages) {
+                override def run(): SimulationSnapshot = {
+                    SimRuntime.initLabelVals()
+
+                    while (currentTurn < totalTurn) {
+                        val mx = collectedMessages.groupBy(_.receiverId)
+                        collectedMessages = actors.filterNot(_.deleted).flatMap(a => {
+                        val targetMessages: List[Message] = a.getProxyIds.flatMap(id => mx.getOrElse(id, List()))
+                        a.cleanSendMessage
+                            .run(targetMessages)._1
+                        }).toList
+                        proceed()
+                    }
+
+                    Actor.reset
+                    SimulationSnapshot(actors, collectedMessages)
+                }
+            }
+
+            def run(c: SimulationConfig): SimulationSnapshot = {
+                new noRoundBase(c).run()
             }
         }
         StartSimulation[BaseMessagingLayer.type](c)(custRunner)
