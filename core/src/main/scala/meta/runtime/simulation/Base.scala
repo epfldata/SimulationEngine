0318f4c20a7a9aa5f0f28a445b7033efb48e0c60
ZiluTian <zilu.tian@epfl.ch>
2021-12-15 20:08:56 +0100

    Combine the simulation with base runner

--- core/src/main/scala/meta/runtime/simulation/Base.scala
+++ core/src/main/scala/meta/runtime/simulation/Base.scala
@@ -6,39 +6,42 @@ import scala.util.Random
 import meta.runtime.Actor.AgentId
 import meta.API.SimulationSnapshot
 
-class Base(var actors: List[Actor], val totalTurn: Int, val messages: List[Message]) extends Simulation {
+class Base(var actors: List[Actor], val totalTurn: Int, val messages: List[Message]) extends Serializable {
 
-  var collectedMessages: List[Message] = messages
+    var currentTurn: Int = 0
+    var collectedMessages: List[Message] = messages
 
-  val events: ListBuffer[()=> Unit] = new ListBuffer()
-  events.append(
-    () => { println(util.displayTime(currentTurn)) }
-  )
-
-  events.append(() => collect())
-
-  events.append(() => {
-    val mx = collectedMessages.groupBy(_.receiverId)
-    
-    collectedMessages = actors.filterNot(_.deleted).flatMap(a => {
-      val targetMessages: List[Message] = a.getProxyIds.flatMap(id => mx.getOrElse(id, List()))
-      a.cleanSendMessage
-        .run(targetMessages)._1
-    }).toList
-  })
-
-  events.append(() => proceed())
-
-  override def run(): SimulationSnapshot = {
-    init()
-
-    while (currentTurn < totalTurn) {
-      util.bench {
-        events.foreach(_())
-      }
+    // discover the newly generated agents
+    def collect() = {
+      actors = SimRuntime.newActors.toList ::: actors
+      SimRuntime.newActors.clear()
     }
 
-    Actor.reset
-    SimulationSnapshot(actors, collectedMessages)
-  }
+    // go to the next state of the Sim
+    def proceed() = {
+      SimRuntime.proceedGroups()
+      currentTurn += 1
+      // currentTurn += proceedLabel("turn").asInstanceOf[Int]
+    }
+
+    def run(): SimulationSnapshot = {
+      SimRuntime.initLabelVals()
+
+      while (currentTurn < totalTurn) {
+        util.bench {
+          println(util.displayTime(currentTurn))
+          collect()
+          val mx = collectedMessages.groupBy(_.receiverId)
+          collectedMessages = actors.filterNot(_.deleted).flatMap(a => {
+            val targetMessages: List[Message] = a.getProxyIds.flatMap(id => mx.getOrElse(id, List()))
+            a.cleanSendMessage
+              .run(targetMessages)._1
+          }).toList
+          proceed()
+        }
+      }
+
+      Actor.reset
+      SimulationSnapshot(actors, collectedMessages)
+    }
 }
