8692a6e2aabc4acf6b0540ba610d1ecdae557299
ZiluTian <zilu.tian@epfl.ch>
2021-12-17 19:50:00 +0100

    More robust testing for lifter

--- core/src/test/scala/lifterMethodTest2.scala
+++ core/src/test/scala/lifterMethodTest2.scala
@@ -11,6 +11,7 @@ import scala.util.Random
 import meta.runtime.Future
 import meta.compile._
 
+@lift
 case class School(val name: String){
     def schoolName(): String = name
 }
@@ -20,6 +21,8 @@ class MyClass3(var s: School) extends Actor {
 
     val c: String = "Hello"
     
+    def hello(): String = c
+
     def main(): Unit = {
         asyncMessage[String](() => s.schoolName())
         println("Hello world!")
@@ -30,8 +33,20 @@ class MyClass3(var s: School) extends Actor {
 @lift
 class MyClass4() extends Actor {
     
+    def hello(): String = "world"
+
     def main(): Unit = {
         println("Hello world!")
+        handleMessages
+    }
+}
+
+
+@lift
+class MyClass5(var s: MyClass4) extends Actor {
+    
+    def main(): Unit = {
+        asyncMessage[String](() => s.hello())
     }
 }
 
@@ -39,17 +54,32 @@ class lifterMethodTest2 extends FlatSpec with org.scalatest.Matchers {
     import meta.deep.IR.Predef._
     import meta.classLifting.Lifter
 
-    "A minimal agent without any method" should "compile" in {
-        val liftMyClass: ClassWithObject[MyClass4] = MyClass4.reflect(IR)
-        new Lifter().apply(List(liftMyClass))
+    val liftSchool = School.reflect(IR)
+    val liftMyClass4: ClassWithObject[MyClass4] = MyClass4.reflect(IR)
+    val liftMyClass3: ClassWithObject[MyClass3] = MyClass3.reflect(IR)
+    val liftMyClass5: ClassWithObject[MyClass5] = MyClass5.reflect(IR)
+
+    "A minimal agent" should "compile" in {    
+        new Lifter().apply(List(liftMyClass4))
     }
 
-    val thrown = the [java.lang.Exception] thrownBy {
-        val liftMyClass3: ClassWithObject[MyClass3] = MyClass3.reflect(IR)
-        new Lifter().apply(List(liftMyClass3))
+    "Lifting a non-agent" should "not compile" in {
+        assertDoesNotCompile("new Lifter().apply(List(liftSchool))")
     }
 
     "An invalid receiver in async message" should "trigger an exception" in {
-        thrown.getMessage should startWith ("Error in asyncMessage!")
+        intercept[java.lang.Exception] {
+            new Lifter().apply(List(liftMyClass3))
+        }
+    }
+
+    "Forget to lift receiver agent" should "trigger an exception" in {
+        intercept[java.lang.Exception] {
+            new Lifter().apply(List(liftMyClass5))
+        }
+    }
+
+    "Valid receiver with proper liftings" should "compile" in {
+        new Lifter().apply(List(liftMyClass5, liftMyClass4))
     }
 }
