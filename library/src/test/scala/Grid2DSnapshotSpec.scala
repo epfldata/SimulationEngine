b1ee4b6676fce9ce1b4edcefe11c340facd29203
ZiluTian <ztian@wpi.edu>
2022-01-11 20:49:40 +0100

    Move the 2d snapshot test file to lib gui

--- library/src/test/scala/Grid2DSnapshotSpec.scala
+++ library/src/test/scala/Grid2DSnapshotSpec.scala
@@ -1,12 +1,12 @@
-package GUI
+package lib
 package test
 
-import GUI.Graphics._
+import Grid._
 
 class Grid2DSnapshotScalaSpec extends org.scalatest.FlatSpec {
 
   "Serialize a 2d grid snapshot" should "return a list of ints" in {
-    val c = Grid2DSnapshotScala(List(1, 0, 1, 0), 2, 2, Map(1 -> "black"))
+    val c = new Grid2DSnapshot(List(1, 0, 1, 0), 2, 2, Map(1 -> "black"))
     println(c.serialize())
   }
 }
