b9877ba2dfa00cf82ee37a29c69992602cc167ae
ZiluTian <zilu.tian@epfl.ch>
2022-01-24 01:19:20 +0100

    Fix initialization error

--- example/src/main/scala/example/cyberSpace/Example.scala
+++ example/src/main/scala/example/cyberSpace/Example.scala
@@ -9,32 +9,23 @@ object MainInit {
             val participantRatio = 0.09
             val superUserRatio = 0.01
             
-            val people = new ListBuffer[Actor]()
-
             val servers = (1 to totalServers).toList.map(x => new Server(syncPeriod))
 
             servers.foreach(s => s.allServers = servers)
 
-            people.appendAll(servers)
-
             val perServerPopulation = populationSize / totalServers
 
-            servers.foreach(x => {
-                people.appendAll(
-                    (0 to (perServerPopulation * readerRatio).toInt).map(r => {
+            servers.flatMap(x => {
+                    x :: ((0 to (perServerPopulation * readerRatio).toInt).map(r => {
                         new Reader(x)
-                    }))
-                people.appendAll(
+                    }) ++
                     (0 to (perServerPopulation * participantRatio).toInt).map(r => {
                         new Participant(x)
-                    }))
-                people.appendAll(
+                    }) ++
                     (0 to (perServerPopulation * superUserRatio).toInt).map(r => {
                         new SuperUser(x)
-                    }))
+                    })).toList
             })
-
-            people.toList
         }
     }
 }
