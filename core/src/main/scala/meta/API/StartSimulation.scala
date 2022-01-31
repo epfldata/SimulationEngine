8147ace3344adb0da5ebeb5569d5411dcbffba2c
ZiluTian <zilu.tian@epfl.ch>
2022-01-31 18:13:47 +0100

    Remove irrelevant dependency

--- core/src/main/scala/meta/API/StartSimulation.scala
+++ core/src/main/scala/meta/API/StartSimulation.scala
@@ -1,7 +1,5 @@
 package meta.API
 
-import scala.collection.mutable.StringBuilder
-
 object StartSimulation {
   def apply[T](c: SimulationConfig)(implicit runner: SimsRunner[T]): SimulationSnapshot = {
       runner.run(c)
