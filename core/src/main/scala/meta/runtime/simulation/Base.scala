0b0719388d71a96ac13c84afc8ca3173a5c71f3d
ZiluTian <ztian@wpi.edu>
2022-01-31 15:18:43 +0100

    Discover agents at the end of round in base

--- core/src/main/scala/meta/runtime/simulation/Base.scala
+++ core/src/main/scala/meta/runtime/simulation/Base.scala
@@ -11,34 +11,30 @@ class Base(var actors: List[Actor], val totalTurn: Int, val messages: List[Messa
     var currentTurn: Int = 0
     var collectedMessages: List[Message] = messages
 
+    def proceed(elapsedTurn: Int = 1) = {
+      currentTurn += elapsedTurn
+    }
+
     // discover the newly generated agents
     def collect() = {
       actors = SimRuntime.newActors.toList ::: actors
       SimRuntime.newActors.clear()
     }
 
-    // go to the next state of the Sim
-    def proceed() = {
-      SimRuntime.proceedGroups()
-      currentTurn += 1
-      // currentTurn += proceedLabel("turn").asInstanceOf[Int]
-    }
-
     def run(): SimulationSnapshot = {
-      SimRuntime.initLabelVals()
-
       while (currentTurn < totalTurn) {
         println(util.displayTime(currentTurn))
-        collect()
         val mx = collectedMessages.groupBy(_.receiverId)
-        collectedMessages = actors.filterNot(_.deleted).flatMap(a => {
+        val res = actors.filterNot(_.deleted).map(a => {
           val targetMessages: List[Message] = a.getProxyIds.flatMap(id => mx.getOrElse(id, List()))
-          a.run(targetMessages)._1
-        }).toList
-        proceed()
+          a.run(targetMessages)
+        }).foldLeft((List[Message](), 1))((a, b) => ((a._1 ::: b._1), if (a._2 > b._2) a._2 else b._2))
+        collect()
+        collectedMessages = res._1
+        proceed(res._2)
       }
 
-      Actor.reset
+      // Actor.reset
       SimulationSnapshot(actors, collectedMessages)
     }
 }
