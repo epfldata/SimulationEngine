b6257baac66aa1ccc7da874e0e22044771bdbe4b
ZiluTian <zilu.tian@epfl.ch>
2022-04-21 21:22:56 +0200

    Add complete bipartite to graph lib

--- library/src/main/scala/Graph/Graph.scala
+++ library/src/main/scala/Graph/Graph.scala
@@ -6,32 +6,35 @@ import lib.Grid.Torus2D
 
 class Graph {
     def build[T](nodes: List[T], f: T=>Unit): Unit = {
-        nodes.foreach(vertex => f(vertex))
+        nodes.foreach(n => f(n))
     }
 }
 
 object FullyConnectedGraph extends Graph {
-    def apply(nodes: List[Actor]) {
-        nodes.foreach(vertex => {
-            vertex.connectedAgents = nodes.filter(_!=vertex)
-        })
+    def apply(nodes: List[Actor]): Unit = {
+        build[Actor](nodes, n=>n.connectedAgents = nodes.filter(_!=n))
     }
 }
 
 object ErdosRenyiGraph extends Graph {
     def apply(nodes: List[Actor], edgeProb: Double): Unit = {
-        nodes.foreach(vertex => {
-            vertex.connectedAgents = nodes.filter(x => {
-                x !=vertex && edgeProb>Random.nextDouble()
-            })
-        })
+        build[Actor](nodes, n => n.connectedAgents = nodes.filter(x => {
+                x !=n && edgeProb>Random.nextDouble()
+            }))
     }
 }
 
 object Torus2DGraph extends Graph {
     def apply(nodes: List[Actor], width: Int, height: Int, radius: Int): Unit = {
-        (1 to nodes.length).foreach(i =>
-            nodes(i-1).connectedAgents = Torus2D.getNeighborCells(width, height)(i-1, radius).map(j => nodes(j))
+        (1 to nodes.length).foreach(n =>
+            nodes(n-1).connectedAgents = Torus2D.getNeighborCells(width, height)(n-1, radius).map(j => nodes(j))
         )
+    }
+}
+
+object CompleteBipartiteGraph extends Graph {
+    // Each vertex in the set nodesA is connected to each vertex in nodesB
+    def apply(nodesA: List[Actor], nodesB: List[Actor]): Unit = {
+        build[Actor](nodesA, n => n.connectedAgents = nodesB)
     }
 }
