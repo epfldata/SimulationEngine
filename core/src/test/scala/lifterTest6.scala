4f1469f9cb8ffb07febee01821738a845781d805
ZiluTian <zilu.tian@epfl.ch>
2022-05-06 10:24:51 +0200

    Add an example for async messaging involving a copied method

--- core/src/test/scala/lifterTest6.scala
+++ core/src/test/scala/lifterTest6.scala
@@ -13,13 +13,29 @@ import org.scalatest.FlatSpec
  */
 
 @lift
-class CommunicatingVehicle(val n: ShortDistanceTransport) extends Vehicle {
+class CommunicatingVehicleType2() extends Vehicle {
+    def override_getPrice(): Int = {
+        println("Received a message for get price in Commmunicating vehicle type 2!")
+        30
+    }
 
+    override def main(): Unit = {
+        while (true) {
+            waitAndReply(1)
+        }
+    }
+}
+
+@lift
+class CommunicatingVehicle(val n: ShortDistanceTransport, val n2: Vehicle) extends Vehicle {
     override def main(): Unit = {
         while (true) {
             if (n != null) {
                 val x = n.getPrice()
                 println("The price for a short distance transport vehicle is " + x + " should change")
+            }
+            if (n2!=null) {
+                asyncMessage(() => n2.getPrice())
             }
             waitAndReply(1)
         }
@@ -36,19 +52,22 @@ class lifterTest6 extends FlatSpec {
         val vehicleClass: ClassWithObject[Vehicle] = Vehicle.reflect(IR)
         val shortDistanceClass: ClassWithObject[ShortDistanceTransport] = ShortDistanceTransport.reflect(IR)
         val cVehicleClass: ClassWithObject[CommunicatingVehicle] = CommunicatingVehicle.reflect(IR)
+        val cVehicleClass2: ClassWithObject[CommunicatingVehicleType2] = CommunicatingVehicleType2.reflect(IR)
 
         val liftedMain = meta.classLifting.liteLift {
             def apply(): List[Actor] = {
                 val s = new ShortDistanceTransport()
-                val c = new CommunicatingVehicle(s)
-                List(s, c)
+                val c2 = new CommunicatingVehicleType2()
+                val c = new CommunicatingVehicle(s, c2)
+                List(s, c, c2)
             }
         }
 
         compileSims(List(
             vehicleClass, 
             shortDistanceClass, 
-            cVehicleClass), 
+            cVehicleClass,
+            cVehicleClass2), 
             mainInit = Some(liftedMain), 
             initPkgName = Some(this.getClass().getPackage().getName()+".inheritance4"),
             destFolder = "core/src/test/scala/generated/inheritance4/")
