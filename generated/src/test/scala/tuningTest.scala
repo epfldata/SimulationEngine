d352a24c46a0af88b71a6e0ca9e1790cd1d24a63
ZiluTian <zilu.tian@epfl.ch>
2022-06-30 18:14:14 +0200

    Remove redundant parentheses

--- generated/src/test/scala/tuningTest.scala
+++ generated/src/test/scala/tuningTest.scala
@@ -9,15 +9,13 @@ class gameOfLifeTuneSpark extends StaticPartitionTest[SparkMessagingLayer.type](
     "gameOfLife", 30, Set(1), Set(0), 
     List(Set(5, 10, 15), Set(100), Set(1)), 
     generated.example.gameOfLife.InitData.wrapper, 
-    generated.example.gameOfLife.InitData.writeSchema) {
-}
+    generated.example.gameOfLife.InitData.writeSchema) 
 
 class gameOfLifeTuneAkka extends StaticPartitionTest[AkkaMessagingLayer.type](
     "gameOfLife", 30, Set(1), Set(0), 
     List(Set(5, 10, 15), Set(100), Set(1)), 
     generated.example.gameOfLife.InitData.wrapper, 
-    generated.example.gameOfLife.InitData.writeSchema) {
-}
+    generated.example.gameOfLife.InitData.writeSchema) 
 
 class gameOfLifeTuneSparkTimeseries extends TimeseriesTest[SparkMessagingLayer.type, List[Boolean]](
     "gameOfLife", 30,  
