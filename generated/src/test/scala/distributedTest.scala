99a79dc44646563376605dff9733ac5fb482a3d4
ZiluTian <zilu.tian@epfl.ch>
2022-06-30 14:14:44 +0200

    Add config that specifies total machines

--- generated/src/test/scala/distributedTest.scala
+++ generated/src/test/scala/distributedTest.scala
@@ -10,17 +10,19 @@ object gameOfLifeDist {
         val name: String = "gameOfLife"
         val latency: Int = 1
         val hostRole = args(0)
-        val hostPort: Int = args(1).toInt
-        val container: Int = args(2).toInt
+        val totalMachines: Int = args(1).toInt
+        val machineSeq: Int = args(2).toInt
+        val hostPort: Int = args(3).toInt
+        val container: Int = args(4).toInt
         
-        val agents = generated.example.gameOfLife.InitData(100, 1000, 1)
+        val agents = generated.example.gameOfLife.InitData(10, 100, 1)
         val output: String = "gameOfLife_dist.csv"
         val pw = new PrintWriter(new FileOutputStream(new File(output),false))
 
         pw.write("Experiment,Containers,K,AvgTime")
         pw.write("\n")
 
-        val c = new SimulationConfig(agents, totalTurn = 5, latencyBound = latency, role=hostRole, port=hostPort)
+        val c = (new DistSimulationConfig(agents, totalTurn = 100, totalMachines, machineSeq, latencyBound = latency, role=hostRole, port=hostPort)).getConfig()
         
         val avgTime = if (container == 0){
             StartSimulation.benchAvg[AkkaMessagingLayer.type](c)
