801ed6826c7f86ed03b6d1d2f18c4b022fe49b1a
ZiluTian <zilu.tian@epfl.ch>
2022-08-29 12:35:44 +0200

    Increase the number of testing agents

--- gen-core/src/test/scala/resetTest.scala
+++ gen-core/src/test/scala/resetTest.scala
@@ -15,13 +15,13 @@ class ResetSimTest extends FlatSpec {
         val agents = generated.core.test.resetSim.InitData()
         val c = new SimulationConfig(agents, 5)
         val r = StartSimulation[BaseMessagingLayer.type](c)
-        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 12))
+        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 28))
         // Reset the Sim except for connectedAgents
         agents.foreach(x => x.SimReset(Set("connectedAgents")))
         agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 0))
 
         val r3 = StartSimulation[BaseMessagingLayer.type](new SimulationConfig(agents, 5))
-        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 12))
+        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 28))
     }
 
 
@@ -29,27 +29,27 @@ class ResetSimTest extends FlatSpec {
         val agents = generated.core.test.resetSim.InitData()
         val c = new SimulationConfig(agents, 5)
         val r = StartSimulation[AkkaMessagingLayer.type](c)
-        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 12))
+        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 28))
         // Reset the Sim except for connectedAgents
         agents.foreach(x => x.SimReset(Set("connectedAgents")))
         agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 0))
         
         // Run another simulation from cloner won't change the state of clonee
         val r3 = StartSimulation[AkkaMessagingLayer.type](new SimulationConfig(agents, 5))
-        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 12))
+        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 28))
     }
 
     "SimReset" should "update the agent in-place in DriverWorkerAkka" in {
         val agents = generated.core.test.resetSim.InitData()
         val c = new SimulationConfig(agents, 5)
         val r = StartSimulation[AkkaDriverWorker.type](c)
-        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 12))
+        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 28))
         // Reset the Sim except for connectedAgents
         agents.foreach(x => x.SimReset(Set("connectedAgents")))
         agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 0))
         
         // Run another simulation from cloner won't change the state of clonee
         val r3 = StartSimulation[AkkaDriverWorker.type](new SimulationConfig(agents, 5))
-        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 12))
+        agents.foreach(a => assert(a.asInstanceOf[generated.core.test.resetSim.Vertex].counter == 28))
     }
 }
