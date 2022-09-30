37a9a4d1618336a3b2b8186106c6013f15f4cd01
ZiluTian <zilu.tian@epfl.ch>
2022-09-30 10:19:01 +0200

    Add script for generating torus with clock

---
+++ library/src/test/scala/writeEdgeFile.scala
@@ -0,0 +1,39 @@
+package lib
+package test
+
+import java.io._
+
+object writeEdgeFile {
+  def main(args: Array[String]): Unit = {
+   val width: Int = args(0).toInt
+   val height: Int = args(1).toInt
+   val radius: Int = 1
+
+   val pw = new PrintWriter(new FileOutputStream(new File(f"2DTorus_${width*height}.txt"),false))
+   for (i <- Range(0, width*height)){
+      val neighbors = Grid.Torus2D.getNeighborCells(width, height)(i, radius)
+      val neighbor_string = neighbors.map(i => f"[$i,0]").mkString(",")
+      pw.write(f"[$i,0,[$neighbor_string]]\n")
+      pw.flush()
+   }
+   pw.close()
+ }  
+}
+
+object writeEdgeFileWithClock {
+   def main(args: Array[String]): Unit = {
+      val width: Int = args(0).toInt
+      val height: Int = args(1).toInt
+      val radius: Int = 1
+
+      val pw = new PrintWriter(new FileOutputStream(new File(f"2DTorus_${width*height}_clock.txt"),false))
+      pw.write("[0,0,[]]\n")
+      for (i <- Range(0, width*height)){
+         val neighbors = Grid.Torus2D.getNeighborCells(width, height)(i, radius)
+         val neighbor_string = neighbors.map(i => f"[${i+1},0]").mkString(",")
+         pw.write(f"[${i+1},0,[$neighbor_string]]\n")
+         pw.flush()
+      }
+      pw.close()
+   }
+}
