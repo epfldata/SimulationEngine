b853761f261f879d42feaecb76f37fa1c3443951
ZiluTian <zilu.tian@epfl.ch>
2022-10-03 15:27:40 +0200

    Update local testing scripts

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,5 @@ set -e
 
 sbt clean
 sbt "project core; test"
-sbt "project genCore; test"
 sbt "project example; runAll"
-sbt "project genExample; testOnly generated.example.test.generatedExamples"
+sbt "project base; test"
