786bae9d0b98505d59d99fb95ad0f3aaa6002da1
ZiluTian <zilu.tian@epfl.ch>
2022-12-28 18:14:08 +0100

    Spark bug fix

--- Spark/src/main/scala/API/Simulate.scala
+++ Spark/src/main/scala/API/Simulate.scala
@@ -13,45 +13,43 @@ import org.apache.spark.HashPartitioner
 import collection.JavaConverters._
 
 object Simulate { 
-
-  @transient protected lazy val conf: SparkConf =
+    @transient protected lazy val conf: SparkConf = 
     new SparkConf().setMaster("local[*]")
-      .setAppName("TickTalk")
-      .set("spark.driver.memory", "50g")
-      .set("spark.executor.memory", "5g")
-      .set("spark.executor.cores", "48")
-      .set("spark.default.parallelism", "96")
-      .set("spark.hadoop.dfs.replication", "1")
-      // .set("spark.serializer", "akka.serialization.jackson.JacksonJsonSerializer")
-      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      // .set("spark.driver.allowMultipleContexts", "true")
+    .setAppName("TickTalk")
+    .set("spark.driver.memory", "50g")
+    .set("spark.executor.memory", "5g")
+    .set("spark.executor.cores", "48")
+    .set("spark.default.parallelism", "96")
+    .set("spark.hadoop.dfs.replication", "1")
+    // .set("spark.serializer", "akka.serialization.jackson.JacksonJsonSerializer")
+    .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    // .set("spark.driver.allowMultipleContexts", "true")
 
-      conf.registerKryoClasses(Array(classOf[meta.runtime.Message]))
-
-  @transient protected lazy val sc: SparkContext = new SparkContext(conf)
-
-  sc.setLogLevel("ERROR")
-  sc.setCheckpointDir("checkpoint/")
-
-  var currentTurn: Int = 0
-  var collectedMessages = scala.collection.Map[Long, List[Message]]()
-
-  // var totalPartitions: Int = 20
-  // .map(a => (a.id, a))
-  // val partitioner = new HashPartitioner(70)
-  // val partitioner = new RangePartitioner(totalPartitions, pair_actors)
-  // print(actorRDD.glom().collect().map(i => i.map(j => j._1).toList).toList)
-  // var actorRDD: RDD[(Long, Actor)] = pair_actors.partitionBy(partitioner).persist()
-    
-
-  var t1: Long = 0
-  var t2: Long = 0
-
-  var totalTime: Long = 0
-  var elapsedRound: Int = 0
-  var time_seq: List[Long] = List()
+    conf.registerKryoClasses(Array(classOf[meta.runtime.Message]))
+    @transient protected lazy val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+    sc.setCheckpointDir("checkpoint/")
 
   def apply(actors: List[Actor], totalTurn: Int): SimulationSnapshot = {
+
+    var currentTurn: Int = 0
+    var collectedMessages = scala.collection.Map[Long, List[Message]]()
+
+    // var totalPartitions: Int = 20
+    // .map(a => (a.id, a))
+    // val partitioner = new HashPartitioner(70)
+    // val partitioner = new RangePartitioner(totalPartitions, pair_actors)
+    // print(actorRDD.glom().collect().map(i => i.map(j => j._1).toList).toList)
+    // var actorRDD: RDD[(Long, Actor)] = pair_actors.partitionBy(partitioner).persist()
+      
+
+    var t1: Long = 0
+    var t2: Long = 0
+
+    var totalTime: Long = 0
+    var elapsedRound: Int = 0
+    var time_seq: List[Long] = List()
+
     var actorRDD: RDD[Actor] = sc.parallelize(actors)
 
     while (currentTurn < totalTurn ) {
@@ -62,22 +60,27 @@ object Simulate {
         }
 
         t1 = System.currentTimeMillis()
-        
-        actorRDD.persist()
-        
+
         collectedMessages = actorRDD.flatMap(a => {
           a.sendMessages.map(i => (i._1, i._2.toList))
         }).combineByKey((msgs: List[Message]) => msgs,
           (l: List[Message], message: List[Message]) => message ::: l,
           (l1: List[Message], l2: List[Message]) => l1 ::: l2).collectAsMap()
-        
-        elapsedRound = actorRDD.map(x => {
-          x.receivedMessages.addAll(collectedMessages.getOrElse(x.id, Buffer[Message]()).asJava)
+
+        // println(collectedMessages)
+        val dMessages = sc.broadcast(collectedMessages)
+
+        actorRDD = actorRDD.map(x => {
+          x.receivedMessages.addAll(dMessages.value.getOrElse(x.id, Buffer[Message]()).asJava)
           x.time += elapsedRound
           x.run()
-        }).collect().min
-        
+          x
+        })
+        actorRDD.cache()
+        elapsedRound = actorRDD.collect().map(i => i.proposeInterval).min
         currentTurn += elapsedRound
+        // actorRDD.collect()
+        // currentTurn += 1
         t2 = System.currentTimeMillis()
         time_seq = time_seq ::: List(t2-t1)
     }
@@ -86,6 +89,7 @@ object Simulate {
     val snapshot = SimulationSnapshot(updatedActors, collectedMessages.flatMap(i => i._2).toList)
     val average = time_seq.sum / time_seq.length
     val stdev = Math.sqrt(time_seq.map(i => (i-average)*(i-average)).sum/time_seq.length)
+    println(f"Time sequence ${time_seq}")
     println(f"Average time per round ${average} StdDev ${stdev}")
     sc.stop()
     snapshot
