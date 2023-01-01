281fc95c53edb2637392540abf4baa348859f657
ZiluTian <zilu.tian@epfl.ch>
2023-01-01 18:12:26 +0100

    Update test file for stock market

--- Akka/src/test/scala/stockMarketTest.scala
+++ Akka/src/test/scala/stockMarketTest.scala
@@ -1,7 +1,7 @@
 package simulation.akka
 package test
 
-object stockMarketTest {
+object stockMarket {
     def main(args: Array[String]): Unit = {
         val totalMarkets = args(0).toInt
         val tradersPerMarket: Int = args(1).toInt
@@ -22,6 +22,13 @@ object stockMarketTest {
                 // v2
                 val cfreq: Int = args(4).toInt
                 val agents = generated.example.stockMarket.v2.InitData(totalMarkets, tradersPerMarket, cfreq)
+                API.OptimizationConfig.mergedWorker()
+                val snapshot1 = API.Simulate(agents, totalTurns)
+            }
+
+            case 3 => {
+                // v3
+                val agents = generated.example.stockMarket.v3.InitData(totalMarkets, tradersPerMarket)
                 API.OptimizationConfig.mergedWorker()
                 val snapshot1 = API.Simulate(agents, totalTurns)
             }
