2c822edde4565a4474c24b9b49cbc8499d4b21cc
ZiluTian <zilu.tian@epfl.ch>
2023-01-01 17:48:32 +0100

    Update stock vertex state with array of arrays

--- example/src/test/scala/writeStockMarketEdgeFile.scala
+++ example/src/test/scala/writeStockMarketEdgeFile.scala
@@ -8,14 +8,15 @@ object writeStockMarketEdgeFile {
 
    val pw = new PrintWriter(new FileOutputStream(new File(f"stockMarket_${markets}_${tradersPerMarket}.txt"),false))
    var market_id: Int = 0
+
    Range(0, markets).foreach(_ => {
       val traders = Range(market_id+1, market_id+1+tradersPerMarket)
       // market 
-      pw.write(f"[$market_id,0,[${traders.map(i => f"[$i,0]").mkString(",")}]]\n")
+      pw.write(f"[$market_id,[[],[],[],[],[]],[${traders.map(i => f"[$i,0]").mkString(",")}]]\n")
       pw.flush()
       // traders. Each trader is connected to its market
       traders.foreach(t => {
-         pw.write(f"[$t,0,[$market_id,0]]\n")
+         pw.write(f"[$t,[[],[],[],[],[]],[[$market_id,0]]]\n")
          pw.flush()
       })
       market_id = market_id+tradersPerMarket+1
