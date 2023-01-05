8468034a63f3550de3439d3c67501b775eca1d19
ZiluTian <zilu.tian@epfl.ch>
2023-01-05 18:02:00 +0100

    Add scripts for generating edge file with clock

--- example/src/test/scala/writeERMEdgeFile.scala
+++ example/src/test/scala/writeERMEdgeFile.scala
@@ -26,3 +26,32 @@ object writeERMEdgeFile {
    pw.close()
  }  
 }
+
+object writeERMEdgeFileWithClock {
+   def main(args: Array[String]): Unit = {
+      val p: Double = args(0).toDouble
+      val vertices: Int = args(1).toInt
+
+      val pw = new PrintWriter(new FileOutputStream(new File(f"ERM_${p}_${vertices}.txt"),false))
+      
+      val clockId = 0
+      val agentIds = Range(1, vertices+1)
+
+      // Initialize it during the simulation
+      val vertexValue = List(0, 0, 0, 0, 0)
+
+      // [vertex id, vertex value, [[dest id, edge value], [dest id, edge value], ...]]
+      // generate the clock vertex, connected to every vertex, including itself
+      pw.write(f"[0,[${vertexValue.mkString(",")}],[${(clockId :: agentIds.toList).map(i => f"[$i,0]").mkString(",")}]]\n")
+
+      // generate agent vertex
+      agentIds.foreach(a => {
+         val neighbors = agentIds.filter(i => {
+            (i!=a) && (p > Random.nextDouble())
+         })
+         pw.write(f"[$a,[${vertexValue.mkString(",")}],[${neighbors.map(i => f"[$i,0]").mkString(",")}]]\n")
+         pw.flush()
+      })    
+      pw.close()
+   }
+}
--- example/src/test/scala/writeSBMEdgeFile.scala
+++ example/src/test/scala/writeSBMEdgeFile.scala
@@ -19,15 +19,45 @@ object writeSBMEdgeFile {
     val verticesPerBlock: Int = (vertices / blocks).toInt
 
    // [vertex id, vertex value, [[dest id, edge value], [dest id, edge value], ...]]
-   agentIds.foreach(a => {
-      val b = (a / verticesPerBlock).toInt
-      val neighbors = agentIds.slice(b*vertices/blocks, (b+1)*vertices/blocks).filter(i => {
-         (i!=a) && (p > Random.nextDouble())
+   Range(0, blocks).foreach(i => {
+      agentIds.slice(i*vertices/blocks, (i+1)*vertices/blocks).foreach(j => {
+         val neighbors = Range(i*vertices/blocks, (i+1)*vertices/blocks).filter(k => {(k!= j) &&  Random.nextDouble() < p})
+         pw.write(f"[$j,[${vertexValue.mkString(",")}],[${neighbors.map(i => f"[$i,0]").mkString(",")}]]\n")
+         pw.flush()
       })
-      pw.write(f"[$a,[${vertexValue.mkString(",")}],[${neighbors.map(i => f"[$i,0]").mkString(",")}]]\n")
-      pw.flush()
-   })
- 
+   }) 
    pw.close()
  }  
+}
+
+object writeSBMEdgeFileWithClock {
+   def main(args: Array[String]): Unit = {
+      val p: Double = args(0).toDouble
+      val vertices: Int = args(1).toInt
+      val blocks: Int = args(2).toInt
+
+      val pw = new PrintWriter(new FileOutputStream(new File(f"SBM_${p}_${vertices}.txt"),false))
+      
+      val clockId: Int = 0
+      val agentIds = Range(1, vertices+1)
+
+      // Initialize it during the simulation
+      val vertexValue = List(0, 0, 0, 0, 0)
+
+      val verticesPerBlock: Int = (vertices / blocks).toInt
+
+      // [vertex id, vertex value, [[dest id, edge value], [dest id, edge value], ...]]
+      // write the clock vertex
+      pw.write(f"[$clockId,[${vertexValue.mkString(",")}],[${(clockId :: agentIds.toList).map(i => f"[$i,0]").mkString(",")}]]\n")
+      
+      Range(0, blocks).foreach(i => {
+         agentIds.slice(i*vertices/blocks, (i+1)*vertices/blocks).foreach(j => {
+            val neighbors = Range(i*vertices/blocks, (i+1)*vertices/blocks).filter(k => {(k!= j) &&  Random.nextDouble() < p})
+            pw.write(f"[$j,[${vertexValue.mkString(",")}],[${neighbors.map(i => f"[$i,0]").mkString(",")}]]\n")
+            pw.flush()
+         })
+      }) 
+   
+      pw.close()
+   }
 }
