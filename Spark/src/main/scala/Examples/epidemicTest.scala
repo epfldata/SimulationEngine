5a86c940f4bec59d7270dae87e7a42dc26d0a5e5
ZiluTian <zilu.tian@epfl.ch>
2023-01-13 15:16:40 +0100

    Update epidemic test in Spark (remove double)

--- Spark/src/main/scala/Examples/epidemicTest.scala
+++ Spark/src/main/scala/Examples/epidemicTest.scala
@@ -4,11 +4,11 @@ package examples
 object epidemic {
     def main(args: Array[String]): Unit = {
         val population: Int = args(0).toInt
-        val p: Double = args(1).toDouble
-        val isSBM: Boolean = (args(2).toInt == 1)
-        val blocks: Int = args(3).toInt
-        val totalTurns: Int = args(4).toInt
-        val mode: Int = args(5).toInt
+        val p: Double = 0.01
+        val isSBM: Boolean = (args(1).toInt == 1)
+        val blocks: Int = args(2).toInt
+        val totalTurns: Int = args(3).toInt
+        val mode: Int = args(4).toInt
 
         mode match {
             case 1 => {
