666062e2a4169f2fe3ba12e15bae95568512428d
ZiluTian <zilu.tian@epfl.ch>
2023-02-25 13:01:52 +0100

    Add gol v2 for Graphicx, each vertex handles own msgs

--- Spark/src/main/scala/GraphxExamples/gameOfLifeGraphx.scala
+++ Spark/src/main/scala/GraphxExamples/gameOfLifeGraphx.scala
@@ -52,3 +52,52 @@ object GameOfLifeGraphx {
     sc.stop()
   }
 }
+
+object GameOfLifeV2Graphx { 
+  def main(args: Array[String]): Unit = {
+    val cores = args(0)
+    val edgeListFile: String = args(1)
+
+    // Creates a SparkSession.
+    val spark = new SparkConf().setMaster(f"local[${cores}]")
+    .setAppName("GameOfLife2")
+    .set("spark.driver.memory", "50g")
+    .set("spark.driver.maxResultSize", "10g")
+    .set("spark.executor.memory", "5g")
+    .set("spark.executor.cores", "48")
+    .set("spark.default.parallelism", "96")
+    .set("spark.hadoop.dfs.replication", "1")
+    // .set("spark.serializer", "akka.serialization.jackson.JacksonJsonSerializer")
+    .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    val sc = new SparkContext(spark)
+    sc.setLogLevel("ERROR")
+    sc.setCheckpointDir("checkpoint/")
+
+    // $example on$
+    // Msg(A): List[Int]
+    // VD: Int
+    // ED: List[Int]
+    // A graph with edge attributes containing distances
+    val graph: Graph[Int, List[Int]] = GraphLoader.edgeListFile(sc, edgeListFile).mapVertices((id, _) => if (Random.nextBoolean()){ 1 } else { 0 }).mapEdges(e => List(e.attr))
+    // Initialize the graph such that each vertex is either alive or dead.
+    // val initialGraph = graph.mapVertices((id, _) => if (Random.nextBoolean()){1} else {0})
+    val gol = graph.pregel(List[Int](0), maxIterations = 200)(
+      (id, alive, receivedMsgs) => {
+        // println("Total received messages " + receivedMsgs.size)
+        val aliveNeighbors = receivedMsgs.filter(i => i == 1).size
+        if (alive==1 && (aliveNeighbors > 3 || aliveNeighbors < 2)) {
+          0
+        } else {
+          1
+        }}, // Vertex Program
+      triplet => {  // Send Message
+          Iterator((triplet.dstId, List(triplet.srcAttr)))
+        },
+      (a, b) => a ::: b // Merge Message
+    )
+    gol.vertices.collect
+    // println(gol.vertices.collect.mkString("\n"))
+    // $example off$
+    sc.stop()
+  }
+}
