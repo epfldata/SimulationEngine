bbdf582d347f7177d29fdc76fedd39ede2e21d00
ZiluTian <zilu.tian@epfl.ch>
2023-03-29 16:18:18 +0200

    Use more efficient data structure for tracking rule strength in stock market example

--- example/src/main/scala/example/stockMarket/conditionActionRule.scala
+++ example/src/main/scala/example/stockMarket/conditionActionRule.scala
@@ -1,104 +1,85 @@
 package example
 package stockMarket
 
-class conditionActionRule {
-    var strength: Double = 0
-    var label: Int = 0
-    def eval(stockPrice: Double, marketState: List[Int]): Int = ???
-    def feedback(success: Boolean): Unit = {
-        if (success){
-            strength += 1
-        } else {
-            strength -= 1
-        }
-    }
+sealed abstract class conditionActionRule() {
+    // 0: no action, 1: buy, 2: sell
+    def eval(stockPrice: Double, marketState: List[Int], cash: Double, shares: Double): Int = ???
 }
 
-// 0: None, 1: true, 2: false
-// Buy if the dividend increases and sell if the dividend decreases
-class rule1(wealth: WealthManagement) extends conditionActionRule {
-    label = 1
-    override def eval(stockPrice: Double, marketState: List[Int]): Int ={
-        if (marketState(0)==1 && stockPrice < wealth.cash){
-            wealth.buyStock(stockPrice)
-            1
-        } else if (marketState(0)==2 && wealth.shares >= 1) {
-            wealth.sellStock(stockPrice)
-            2
+object conditionActionRule {
+    val buy: Int = 1
+    val sell: Int = 2
+    val noAction: Int = 0
+  // Buy if the dividend increases and sell if the dividend decreases
+  final case object Rule1 extends conditionActionRule {
+    override def eval(stockPrice: Double, marketState: List[Int], cash: Double, shares: Double): Int ={
+        if (marketState(0)==1 && stockPrice < cash){
+            buy
+        } else if (marketState(0)==2 && shares >= 1) {
+            sell
         } else {
-            0
+            noAction
         }
-    }
-}
+    }    
+  }
 
-// Buy if 100-day average decreases and sell if 10-day average increases
+  // Buy if 100-day average decreases and sell if 10-day average increases
 // If both conditions are met, prioritize sell
-class rule2(wealth: WealthManagement) extends conditionActionRule {
-    label = 2
-    override def eval(stockPrice: Double, marketState: List[Int]): Int ={
-        if (marketState(1) == 1 && wealth.shares >= 1){
-            wealth.sellStock(stockPrice)
-            2
-        } else if (stockPrice < wealth.cash && marketState(2) == 2){
-            wealth.buyStock(stockPrice)
-            1
+  final case object Rule2 extends conditionActionRule {
+    override def eval(stockPrice: Double, marketState: List[Int], cash: Double, shares: Double): Int ={
+        if (marketState(1) == 1 && shares >= 1){
+            sell
+        } else if (stockPrice < cash && marketState(2) == 2){
+            buy
         } else {
-            0
+            noAction
         }
     }
-}
+  }
 
-// Buy if 10-day average decreases and sell if 10-day average increases
-class rule3(wealth: WealthManagement) extends conditionActionRule {
-    label = 3
-    override def eval(stockPrice: Double, marketState: List[Int]): Int ={
-        if (marketState(1) == 2 && stockPrice < wealth.cash){
-            wealth.buyStock(stockPrice)
-            1
-        } else if (marketState(1) == 1 && wealth.shares >= 1){
-            wealth.sellStock(stockPrice)
-            2
+  // Buy if 10-day average decreases and sell if 10-day average increases
+  final case object Rule3 extends conditionActionRule {
+        override def eval(stockPrice: Double, marketState: List[Int], cash: Double, shares: Double): Int ={
+        if (marketState(1) == 2 && stockPrice < cash){
+            buy
+        } else if (marketState(1) == 1 && shares >= 1){
+            sell
         } else {
-            0
+            noAction
         }
     }
-}
+  }
 
-// Random buy and sell
-class rule4(wealth: WealthManagement) extends conditionActionRule {
-    label = 4
-    override def eval(stockPrice: Double, marketState: List[Int]): Int ={
+    // Random buy and sell
+  final case object Rule4 extends conditionActionRule {
+        override def eval(stockPrice: Double, marketState: List[Int], cash: Double, shares: Double): Int ={
         if (Random.nextBoolean){
-            if (stockPrice < wealth.cash) {
-                wealth.buyStock(stockPrice)
-                1
+            if (stockPrice < cash) {
+                buy
             } else {
-                0
+                noAction
             }
         } else {
-            if (wealth.shares >= 1) {
-                wealth.sellStock(stockPrice)
-                2
+            if (shares >= 1) {
+                sell
             } else {
-                0
+                noAction
             }
         }
     }
-}
+  }
 
-// Buy if 50-day average decreases and sell if 50-day average increases
+  // Buy if 50-day average decreases and sell if 50-day average increases
 // If both conditions are met, prioritize sell
-class rule5(wealth: WealthManagement) extends conditionActionRule {
-    label = 5
-    override def eval(stockPrice: Double, marketState: List[Int]): Int ={
-        if (marketState(2) == 1 && wealth.shares >= 1){
-            wealth.sellStock(stockPrice)
-            2
-        } else if (marketState(2) == 2 && stockPrice < wealth.cash){
-            wealth.buyStock(stockPrice)
-            1
+  final case object Rule5 extends conditionActionRule {
+        override def eval(stockPrice: Double, marketState: List[Int], cash: Double, shares: Double): Int ={
+        if (marketState(2) == 1 && shares >= 1){
+            sell
+        } else if (marketState(2) == 2 && stockPrice < cash){
+            buy
         } else {
-            0
+            noAction
         }
     }
+  }
 }
--- example/src/main/scala/example/stockMarket/wealthManagement.scala
+++ example/src/main/scala/example/stockMarket/wealthManagement.scala
@@ -1,15 +1,20 @@
 package example
 package stockMarket
 
+import conditionActionRule._
 
 class WealthManagement(var initWealth: Double, val interestRate: Double) {
     var bankDeposit = initWealth * 0.5
     var cash = initWealth - bankDeposit
     var shares: Double = 0
-    var currentAction: conditionActionRule = null
-    var currentWealth: Double = initWealth
+    
+    val rules: List[conditionActionRule] = List(Rule1, Rule2, Rule3, Rule4, Rule5)
+    // Strength of the rules
+    val strength: Array[Int] = Array.ofDim[Int](rules.size)
 
-    val rules: Set[conditionActionRule] = Set(new rule1(this), new rule2(this), new rule3(this), new rule4(this), new rule5(this))
+    // index to rules
+    var currentAction: Int = Random.nextInt(rules.size)  // randomly initialize
+    var currentWealth: Double = initWealth
 
     def buyStock(stockPrice: Double): Unit = {
         shares += 1
@@ -42,15 +47,26 @@ class WealthManagement(var initWealth: Double, val interestRate: Double) {
     def takeAction(stockPrice: Double, marketState: List[Int]): Int = {
         // Evaluate the previous rule
         val updatedWealth = estimateWealth(stockPrice)
-        if (currentAction != null){
-            currentAction.feedback(updatedWealth > currentWealth)
+        // Feedback
+        if (updatedWealth > currentWealth) {
+            strength(currentAction) += 1
         }
         // Find the next rule
-        val nextAction: conditionActionRule = rules.map(x => (x.strength, x)).toList.sortBy(x => x._1).head._2
+        // Add some randomness to avoid one dominating action
+        if (Random.nextInt(10) < 3) {
+            currentAction = Random.nextInt(rules.size)
+        } else {
+            currentAction = strength.indexOf(strength.max)
+        }
         // println("Next action is " + nextAction)
         currentWealth = updatedWealth
         // println("Current wealth is " + currentWealth)
-        currentAction = nextAction
-        nextAction.eval(stockPrice, marketState)
+        val action = rules(currentAction).eval(stockPrice, marketState, cash, shares)
+        if (action == buy) {
+            buyStock(stockPrice)
+        } else if (action == sell) {
+            sellStock(stockPrice)
+        }
+        action
     }
 }
