5448718d7723c7fe3d04e38d35dbfcd3d5fcf64c
ZiluTian <zilu.tian@epfl.ch>
2023-04-04 19:28:59 +0200

    Update Snapshot API to simple Traversable

--- core/src/main/scala/meta/API/SimulationSnapshot.scala
+++ core/src/main/scala/meta/API/SimulationSnapshot.scala
@@ -7,8 +7,5 @@ import meta.runtime.{Actor, Message}
   * @param actors the Sims at the current state
   * @param messages the list of in-transit messages
   */
-case class SimulationSnapshot(val sims: List[Actor], val messages: List[Message]=List()){
-  def + (other : SimulationSnapshot) = SimulationSnapshot(
-    sims ::: other.sims,
-    messages ::: other.messages)
+case class SimulationSnapshot(val sims: Traversable[Actor], val messages: Traversable[Message]=List()){
 }
